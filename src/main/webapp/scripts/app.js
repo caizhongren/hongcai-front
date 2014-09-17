63f423f98e78fe9bc0c3c178a3021c3d682fcdb4
laughin <jinxiao@me.com>
2014-09-17 16:43:25 +0800

    bug fix

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,22 +1,26 @@
-define(['angularAMD', 'angular-route', 'angular-resource'], function (angularAMD) {
-	
-    var hongcaiApp = angular.module("hongcaiApp", ['ngRoute', 'ngResource']);
-    
-    hongcaiApp.config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-        .when("/project-list", angularAMD.route({templateUrl: 'views/project/project-list.html', controller: 'ProjectListCtrl', controllerUrl: 'scripts/controller/project/project-list-ctrl'
-        }))
-        .otherwise({redirectTo: '/'});
-        
-        $locationProvider.hashPrefix('!');
-    });
-    
-    hongcaiApp.filter('startFrom', function() {
-        return function(input, start) {
-            start = +start;
-            return input.slice(start);
-        }
-    });
-    
-    return angularAMD.bootstrap(hongcaiApp);
+define([ 'angularAMD', 'angular-route', 'angular-resource' ], function(angularAMD) {
+
+	var hongcaiApp = angular.module("hongcaiApp", [ 'ngRoute','ngResource' ]);
+
+	hongcaiApp.config(function($routeProvider, $locationProvider) {
+				$routeProvider
+				.when("/project-list", 
+						angularAMD.route({
+							templateUrl : 'views/project/project-list.html', 
+							controller : 'ProjectListCtrl', 
+							controllerUrl : 'scripts/controller/project/project-list-ctrl'
+				}))
+				.otherwise({redirectTo : '/'});
+
+				$locationProvider.hashPrefix('!');
+			});
+
+	hongcaiApp.filter('startFrom', function startFrom() {
+		return function(input, start) {
+			return input.slice(parseInt(start));
+		}
+	});
+
+
+	return angularAMD.bootstrap(hongcaiApp);
 });
