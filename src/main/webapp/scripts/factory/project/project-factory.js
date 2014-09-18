1dbeb87050ff4d1b725abb877f1cfc7fa91b4cf0
laughin <jinxiao@me.com>
2014-09-18 16:15:25 +0800

    添加app.js全局常量 DEFAULT_DOMAIN

--- src/main/webapp/scripts/factory/project/project-factory.js
+++ src/main/webapp/scripts/factory/project/project-factory.js
@@ -1,6 +1,5 @@
 define(['scripts/app'], function (hongcaiApp) {
-	hongcaiApp.factory('ProjectFactory', function ($resource, $location) {
-    	var DEFAULT_DOMAIN = "http://" + $location.host() + ":" + $location.port() + "/hongcai/api/v1";
+	hongcaiApp.factory('ProjectFactory', function ($resource, $location, DEFAULT_DOMAIN) {
     	return {
     		projectDetail: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectDetail', {projectId:'@projectId'}),
     		projectList: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectList')
