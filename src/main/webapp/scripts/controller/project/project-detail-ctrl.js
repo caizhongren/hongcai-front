5b7e2be0f11d88af222783b39e43a20d85212e9f
zhangfeng <zhangfeng@hoolai.com>
2014-09-18 14:05:37 +0800

    项目详情

--- src/main/webapp/scripts/controller/project/project-detail-ctrl.js
+++ src/main/webapp/scripts/controller/project/project-detail-ctrl.js
@@ -1,9 +1,21 @@
 define(['scripts/app', 'scripts/factory/project/project-factory'], function(app) {
 
     app.controller("ProjectDetailCtrl", ["$scope", "$routeParams", "ProjectFactory", function ($scope, $routeParams, ProjectFactory) {
-    	$scope.projectId = $routeParams.projectId;
         var projectDetail = ProjectFactory.projectDetail.get({projectId: $routeParams.projectId}, function() {
-        	
+        	var project = projectDetail.data.project;
+        	var projectInfo = projectDetail.data.projectInfo;
+        	var pledges = projectDetail.data.pledges;
+
+        	// 项目
+        	$scope.name = project.name;
+        	$scope.total = project.total;
+        	$scope.annualEarnings = project.annualEarnings;
+        	$scope.cycle = project.cycle;
+        	$scope.description = project.description;
+
+        	// 项目信息
+        	$scope.financingPurpose = projectInfo.financingPurpose;
+        	$scope.repaymentSource = projectInfo.repaymentSource;
     	});
     }]);
 });
