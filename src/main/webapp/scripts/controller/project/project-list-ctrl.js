40eef3e1de6239bed9c1521ebe9755b74fee7d28
laughin <jinxiao@me.com>
2014-09-20 17:38:54 +0800

    增加排序

--- src/main/webapp/scripts/controller/project/project-list-ctrl.js
+++ src/main/webapp/scripts/controller/project/project-list-ctrl.js
@@ -5,8 +5,10 @@ define(['scripts/app', 'scripts/factory/project/project-factory'], function(app)
         												  maxCycle: $routeParams.maxCycle, 
         												  minEarning: $routeParams.minEarning, 
         												  maxEarning: $routeParams.maxEarning, 
-        												  minTotalAmount: $routeParams.minTotalAmount,
-        												  maxTotalAmount: $routeParams.maxTotalAmount,}, function() {
+        												  minTotalAmount: $routeParams.minTotalAmount, 
+        												  maxTotalAmount: $routeParams.maxTotalAmount, 
+        												  sortCondition: $routeParams.sortCondition}, function() {
+        													  console.log($routeParams.sortCondition);
     		$scope.projectList = projectList.data;
     		$scope.status = $routeParams.status;
     		$scope.minCycle = $routeParams.minCycle;
@@ -15,11 +17,12 @@ define(['scripts/app', 'scripts/factory/project/project-factory'], function(app)
     		$scope.maxEarning = $routeParams.maxEarning;
     		$scope.minTotalAmount = $routeParams.minTotalAmount;
     		$scope.maxTotalAmount = $routeParams.maxTotalAmount;
+    		$scope.sortCondition = $routeParams.sortCondition;
     		$scope.orderProp = 'id';
     		$scope.currentPage = 0;
     	    $scope.pageSize = 15;
     	    $scope.data = [];
-    	    $scope.numberOfPages = function(){
+    	    $scope.numberOfPages = function() {
     	        return Math.ceil($scope.data.length / $scope.pageSize);
     	    }
     	    for (var i = 0; i < $scope.projectList.projectList.length; i++) {
--- src/main/webapp/scripts/factory/project/project-factory.js
+++ src/main/webapp/scripts/factory/project/project-factory.js
@@ -2,13 +2,7 @@ define(['scripts/app'], function (hongcaiApp) {
 	hongcaiApp.factory('ProjectFactory', function ($resource, $location, DEFAULT_DOMAIN) {
     	return {
     		projectDetail: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectDetail', {projectId:'@projectId'}), 
-    		projectList: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectList', {status:'@status', 
-    																				minCycle:'@minCycle', 
-    																				maxCycle:'@maxCycle', 
-    																				minEarning: '@minEarning', 
-    																				maxEarning: '@maxEarning', 
-    																				minTotalAmount: '@minTotalAmount', 
-    																				maxTotalAmount: '@maxTotalAmount'})
+    		projectList: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectList')
     	}
     });
 });
