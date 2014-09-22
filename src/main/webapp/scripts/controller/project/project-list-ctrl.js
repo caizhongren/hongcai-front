a83fdf15e116a85b43720e35029291096f9ed5e6
laughin <jinxiao@me.com>
2014-09-22 14:57:57 +0800

    增加排序

--- src/main/webapp/scripts/controller/project/project-list-ctrl.js
+++ src/main/webapp/scripts/controller/project/project-list-ctrl.js
@@ -1,5 +1,5 @@
 define(['scripts/app', 'scripts/factory/project/project-factory'], function(app) {
-    app.controller("ProjectListCtrl", ["$scope", "$routeParams", "ProjectFactory", function ($scope, $routeParams, ProjectFactory) {
+    app.controller("ProjectListCtrl", ["$scope", "$routeParams", "$location", "ProjectFactory", function ($scope, $routeParams, $location, ProjectFactory) {
         var projectList = ProjectFactory.projectList.get({status: $routeParams.status, 
         												  minCycle: $routeParams.minCycle, 
         												  maxCycle: $routeParams.maxCycle, 
@@ -7,8 +7,8 @@ define(['scripts/app', 'scripts/factory/project/project-factory'], function(app)
         												  maxEarning: $routeParams.maxEarning, 
         												  minTotalAmount: $routeParams.minTotalAmount, 
         												  maxTotalAmount: $routeParams.maxTotalAmount, 
-        												  sortCondition: $routeParams.sortCondition}, function() {
-        													  console.log($routeParams.sortCondition);
+        												  sortCondition: $routeParams.sortCondition, 
+        												  sortType: $routeParams.sortType}, function() {
     		$scope.projectList = projectList.data;
     		$scope.status = $routeParams.status;
     		$scope.minCycle = $routeParams.minCycle;
@@ -18,6 +18,7 @@ define(['scripts/app', 'scripts/factory/project/project-factory'], function(app)
     		$scope.minTotalAmount = $routeParams.minTotalAmount;
     		$scope.maxTotalAmount = $routeParams.maxTotalAmount;
     		$scope.sortCondition = $routeParams.sortCondition;
+    		$scope.sortType = $routeParams.sortType;
     		$scope.orderProp = 'id';
     		$scope.currentPage = 0;
     	    $scope.pageSize = 15;
@@ -28,6 +29,11 @@ define(['scripts/app', 'scripts/factory/project/project-factory'], function(app)
     	    for (var i = 0; i < $scope.projectList.projectList.length; i++) {
     	        $scope.data.push($scope.projectList.projectList[i]);
     	    }
+    	    
+    	    $scope.sortType = false;
+    	    $scope.toggleSort = function() {
+    	    	$scope.sortType = !$scope.sortType;
+	    	};
     	});
     }]);
 });
