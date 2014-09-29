75f34b65308fe325130d5ced25ef43da00572183
leopku <leopku@qq.com>
2014-09-29 15:12:41 +0800

    initial import

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name p2pSiteWebApp
+ * @description
+ * # p2pSiteWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('p2pSiteWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name p2pSiteWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the p2pSiteWebApp
+ */
+angular.module('p2pSiteWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
