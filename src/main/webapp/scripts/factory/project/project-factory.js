deaa99d2754cdd7ed67a40501f4c8f50ef248404
leopku <leopku@qq.com>
2014-09-29 14:59:57 +0800

    delete unused files.

--- src/main/webapp/scripts/factory/project/project-factory.js
+++
@@ -1,8 +0,0 @@
-define(['scripts/app'], function (hongcaiApp) {
-	hongcaiApp.factory('ProjectFactory', function ($resource, $location, DEFAULT_DOMAIN) {
-    	return {
-    		projectDetail: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectDetail', {projectId:'@projectId'}), 
-    		projectList: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectList')
-    	}
-    });
-});
