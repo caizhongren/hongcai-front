5f0b717e3eddbb1657725fa2c3a7674154c37252
leopku <leopku@qq.com>
2014-09-30 12:51:39 +0800

    index page done.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,5 +14,39 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('root', {
+        abstract: true,
+        views: {
+          '': {
+            templateUrl: 'views/root.html'
+            // controller: 'DefaultCtrl'
+          },
+          'header': {
+            templateUrl: 'views/header.html'
+          },
+          'slider': {
+            templateUrl: 'views/slider.html'
+          },
+          'footer': {
+            templateUrl: 'views/footer.html'
+          }
+        }
+      })
+      .state('root.main', {
+        url: '/',
+        views: {
+          '': {
+            templateUrl: 'views/main.html'
+          }
+        }
+      })
+      ;
+
+      $urlRouterProvider.otherwise('/');
+  }])
+  ;
