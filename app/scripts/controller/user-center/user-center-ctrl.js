59c4d530d004de84cec40ca8c1ddd68544702ea4
crystal <gisele6@126.com>
2014-10-14 11:53:49 +0800

    dele-tabs

--- app/scripts/controller/user-center/user-center-ctrl.js
+++ app/scripts/controller/user-center/user-center-ctrl.js
@@ -1,44 +1,4 @@
 hongcaiApp.controller("UserCenterCtrl", ["$scope", "$rootScope", "$stateParams", "UserCenterService", function ($scope, $rootScope, $stateParams, UserCenterService) {
-		/*$scope.tabs = [{
-            title: 'One',
-            url: 'one.tpl.html'
-        }, {
-            title: 'Two',
-            url: 'two.tpl.html'
-        }, {
-            title: 'Three',
-            url: 'three.tpl.html'
-    }];
-
-    $scope.currentTab = 'one.tpl.html';
-
-    $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.url;
-    }*/
-	$scope.tabs = [
-	  {
-	    'title': '全部',
-	    'content': ''
-	  },
-	  {
-	    'title': '充值',
-	    'content': ''
-	  },
-	  {
-	    'title': '提现',
-	    'content': ''
-	  },
-	  {
-	    'title': '投资',
-	    'content': ''
-	  },
-	  {
-	    'title': '回款',
-	    'content': ''
-	  }
-	];
-	$scope.tabs.activeTab = 0;
-
 
 	function new_form(){
         var f = document.createElement("form");
