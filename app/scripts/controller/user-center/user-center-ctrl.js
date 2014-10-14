7e95f0f14bba4318eef78cb523e0ffa8c1038b59
laughin <jinxiao@me.com>
2014-10-14 19:05:03 +0800

    Char.js 账户总览

--- app/scripts/controller/user-center/user-center-ctrl.js
+++ app/scripts/controller/user-center/user-center-ctrl.js
@@ -1,11 +1,103 @@
-hongcaiApp.controller("UserCenterCtrl", ["$scope", "$rootScope", "$stateParams", "UserCenterService", function ($scope, $rootScope, $stateParams, UserCenterService) {
-	var username;
-	$scope.username = $rootScope.loginName;
+hongcaiApp.controller("UserCenterCtrl", ["$scope", "$state", "$rootScope", "$stateParams", "UserCenterService", function ($scope, $state, $rootScope, $stateParams, UserCenterService) {
+	
+	UserCenterService.getUserCapital.get(function(response) {
+        if(response.msg == 'success') {
+        	$scope.capital = response.data;
+            $scope.doughnutAccountData = [{
+					value: response.data.totalAssets,
+					label: '账户总资产',
+					//highlight: '#FF5A5E',
+					color:"#e94828"
+				},
+				{
+					value : response.data.userCapital.receivedProfit,
+					label: '累计净收益',
+					color : "#f9b81e"
+				},
+				{
+					value : response.data.userCapital.balance + response.data.userCapital.freezeCapital,
+					label: '账户余额',
+					color : "#62cbc6"
+				}]
+
+			$scope.doughnutAssetsData = [{
+					value : response.data.userCapital.balance,
+					label: '可用余额',
+					color : "#d2cb3f"
+				},
+				{
+					value : response.data.userCapital.waitingProfit,
+					label: '待收收益',
+					color : "#62cbc6"
+				},
+				{
+					value : response.data.userCapital.waitingCapital,
+					label: '待收本金',
+					color : "#f9b81e"
+				},
+				{
+					value : response.data.userCapital.freezeCapital,
+					label: '冻结资金',
+					color : "#6aabe1"
+				},
+				{
+					value: response.data.userCapital.receivedProfit,
+					label: '已收收益',
+					//highlight: '#FF5A5E',
+					color:"#913969"
+				},
+				{
+					value : response.data.userCapital.amount,
+					label: '累计投资',
+					color : "#e94828"
+				}]
+
+				$scope.doughnutOptions = {
+				    //Boolean - Whether we should show a stroke on each segment
+					segmentShowStroke : false,
+
+					//String - The colour of each segment stroke
+					segmentStrokeColor : "#fff",
+					
+					//Number - The width of each segment stroke
+					segmentStrokeWidth : 2,
+					
+					//The percentage of the chart that we cut out of the middle.
+					percentageInnerCutout : 50,
+					
+					//Boolean - Whether we should animate the chart	
+					animation : true,
+					
+					//Number - Amount of animation steps
+					animationSteps : 100,
+					
+					//String - Animation easing effect
+					animationEasing : "easeOutBounce",
+					
+					//Boolean - Whether we animate the rotation of the Doughnut
+					animateRotate : true,
+
+					//Boolean - Whether we animate scaling the Doughnut from the centre
+					animateScale : false,
+					
+					//Function - Will fire on animation completion.
+					onAnimationComplete : null
+			    };
+        } else {
+            //toaster.pop('warning', "提示", response.msg);
+            //$scope.errorMessage = response.msg;
+            //$scope.warning = true;
+            $state.go('root.login');
+        }
+
+    });
+
+	
 
 	//Datepickers 
 	/*$scope.selectedDate = {{selectedDate}}; 
 	$scope.selectedDateAsNumber = {{selectedDateAsNumber}};
 	$scope.fromDate = {{fromDate}}; 
 	$scope.untilDate = {{untilDate}};*/ 
-}]);
 
+}]);
--- app/scripts/service/user-center/user-center-service.js
+++ app/scripts/service/user-center/user-center-service.js
@@ -1,5 +1,6 @@
 hongcaiApp.factory('UserCenterService', function ($resource, DEFAULT_DOMAIN) {
 	return {
-		yeepayRegister: $resource(DEFAULT_DOMAIN + '/yeepay/register', {realName: '@realName', idNo: '@idNo' })
+		yeepayRegister: $resource(DEFAULT_DOMAIN + '/yeepay/register', {realName: '@realName', idNo: '@idNo' }),
+		getUserCapital: $resource(DEFAULT_DOMAIN + '/siteUser/getUserCapitalById')
 	};
 });
