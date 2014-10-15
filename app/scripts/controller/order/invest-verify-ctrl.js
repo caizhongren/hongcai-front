c894f70fc2bfb02b1c7e41a2e845dea52cfcc973
zhangfeng <zhangfeng@hoolai.com>
2014-10-15 21:48:00 +0800

    立即投标

--- app/scripts/controller/order/invest-verify-ctrl.js
+++ app/scripts/controller/order/invest-verify-ctrl.js
@@ -9,5 +9,44 @@ hongcaiApp.controller("investVerifyCtrl", ["$scope", "$state", "$rootScope", "$s
         } 
     });
 
-    
+	function new_form(){
+		var f = document.createElement("form");
+		document.body.appendChild(f);
+		f.method = "post";
+        //f.target = "_blank";
+        return f;
+    }
+
+    function create_elements(eForm,eName,eValue){
+    	var e=document.createElement("input");
+    	eForm.appendChild(e);
+    	e.type='text';
+    	e.name=eName;
+    	if(!document.all){
+    		e.style.display='none';
+    	}else{
+    		e.style.display='block';
+    		e.style.width='0px';
+    		e.style.height='0px';
+    	}
+    	e.value=eValue;
+    	return e;
+    }
+
+    $scope.transfer = function(orderId){
+    	OrderService.transfer.get({orderId: orderId, }, function(response) {
+        	if(response.ret == 1) {
+    			var req = response.data.req;
+    			var sign = response.data.sign;
+             	var _f=new_form();//创建一个form表单
+                create_elements(_f,"req",req);//创建form中的input对象
+                create_elements(_f,"sign",sign);
+                _f.action="http://qa.yeepay.com/member/bha/toTransfer";//form提交地址
+                _f.submit();//提交
+       		} 
+    	});
+
+    }
+
+
 }]);
