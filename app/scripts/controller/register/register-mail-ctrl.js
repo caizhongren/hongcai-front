40e56bef844fc328fd30cb1b6eb2c6a333ac7c26
crystal <gisele6@126.com>
2014-10-18 16:34:36 +0800

    merge

---
+++ app/scripts/controller/register/register-mail-ctrl.js
@@ -0,0 +1,24 @@
+hongcaiApp.controller("RegisterMailCtrl", ["$scope", "$state", "$rootScope", "$stateParams", "RegisterService", "SessionService", "DEFAULT_DOMAIN", "toaster", function ($scope, $state, $rootScope, $stateParams, RegisterService, SessionService, DEFAULT_DOMAIN, toaster) {
+
+    $scope.getPicCaptcha = DEFAULT_DOMAIN + "/siteUser/getPicCaptcha?";
+
+    $scope.submitRegisterMail = function(user) {
+        RegisterService.saveRegister.save({name: user.name, type: 1, account: user.email, password: user.password, captcha: user.captcha }, function(response) {
+            if(response.ret == 1) {
+                SessionService.set("user", response.data.user.name);
+                $state.go('root.userCenter.account-overview');
+                $rootScope.loginName = response.data.user.name;
+                $rootScope.isLogged = true;
+            } else {
+                toaster.pop('warning', "提示", response.msg);
+                $state.go('root.registerMail');
+            }
+        });
+    };
+
+    $scope.refreshCode = function() {
+        angular.element("#checkCaptcha").attr("src", angular.element("#checkCaptcha").attr("src").substr(0, angular.element("#checkCaptcha").attr("src").indexOf('?')) + "?code=" + Math.random());
+    };
+
+}]);
+ 
