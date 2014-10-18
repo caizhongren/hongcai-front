0596a423300bb264193cf5b4b0c8bbfb445fbe37
dongyao <dongyao@hoolai.com>
2014-10-18 21:17:33 +0800

    找回密码

--- app/scripts/controller/get-pwd-back/get-pwd-back-ctrl.js
+++ app/scripts/controller/get-pwd-back/get-pwd-back-ctrl.js
@@ -6,7 +6,7 @@ hongcaiApp
     	{value: 'phone', label: '手机找回'},
     	{value: 'email', label: '邮箱找回'},
   	];
-  	$scope.areaFlag = 1;
+  	$scope.areaFlag = 3;
   	$scope.getPicCaptcha = DEFAULT_DOMAIN + "/siteUser/getPicCaptcha?";
   	$scope.refreshCode = function() {
         angular.element("#checkCaptcha").attr("src", angular.element("#checkCaptcha").attr("src").substr(0, angular.element("#checkCaptcha").attr("src").indexOf('?')) + "?code=" + Math.random());
@@ -14,5 +14,18 @@ hongcaiApp
     $scope.verifyAccount = function(){
       $scope.areaFlag = 2;
     }
+    $scope.setPhoneNewPwd = function(){
+      $scope.areaFlag = 4;
+      $scope.seconds = 5;
+      function timer(){
+        $scope.seconds--;
+      }
+      while($scope.seconds>=0){
+        setInterval("timer()",1000);
+      }
+    }
+}])
+.controller("SetPwdCtrl", ["$scope", "$state", "$rootScope", "$stateParams", "SessionService", "DEFAULT_DOMAIN", "toaster","GetPwdService", function ($scope, $state, $rootScope, $stateParams, SessionService, DEFAULT_DOMAIN, toaster,GetPwdService) {
+    $scope.areaFlag = 3;
 }])
 ;
