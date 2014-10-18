555421f7c0a146fd87d5318969e592d2f2a5c57b
dongyao <dongyao@hoolai.com>
2014-10-18 19:27:53 +0800

    找回密码

--- app/scripts/directives/find-account.js
+++ app/scripts/directives/find-account.js
@@ -11,10 +11,9 @@ angular.module('hongcaiApp').directive('findAccount', ['$http', 'DEFAULT_DOMAIN'
 						url: DEFAULT_DOMAIN + '/siteUser/findAccount?account=' + account
 					}).success(function(data, status, headers, cfg) {
 						if(data.data.user) {
-							console.info(ctrl);
+							scope.usermessage =  data.data.user;
 							ctrl.$setValidity('unique', true);
 						} else if(!data.data.user) {
-							console.info(123);
 							ctrl.$setValidity('unique', false);
 						}
 					}).error(function(data, status, headers, cfg) {
