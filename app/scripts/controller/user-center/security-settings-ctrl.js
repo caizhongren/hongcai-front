5ddbd465b4895706facfef3f41ff94e8c9742b75
dongyao <dongyao@hoolai.com>
2014-10-20 13:44:30 +0800

    细节修改

--- app/scripts/controller/user-center/security-settings-ctrl.js
+++ app/scripts/controller/user-center/security-settings-ctrl.js
@@ -133,26 +133,7 @@ hongcaiApp.controller("SecuritySettingsCtrl", ["$scope", "$state", "$rootScope",
 
             }
         });
-    };
-
-     var wait=60;
-    function setTime(val) {
-        if ( wait == 0 ) {
-            val.removeAttribute("disabled");          
-            val.value = '获取验证码';
-            val.className = 'get-verify-button verify-index';
-            wait = 60;
-        }else{
-            val.setAttribute("disabled", true);
-            val.className = 'get-verify-button verify-index grey';
-            val.value = wait + "s 后重新发送";
-            wait--;
-            setTimeout(function() {
-                time(val)
-            },
-            1000)
-        }
-    }   
+    };   
 }]);
 
 
