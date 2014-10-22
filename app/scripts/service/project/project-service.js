fbe65a6291d6cc81c3037c8c6c677a943d95a232
chenhui <chenhui@hoolai.com>
2014-10-22 19:16:55 +0800

    guarantee

--- app/scripts/service/project/project-service.js
+++ app/scripts/service/project/project-service.js
@@ -3,6 +3,7 @@ hongcaiApp.factory('ProjectService', function ($resource, $location, DEFAULT_DOM
 		projectDetails: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectDetail', {projectId:'@projectId'}), 
 		projectList: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectList'),
 		isAvailableInvest: $resource(DEFAULT_DOMAIN + '/siteProject/isAvailableInvest', {amount: '@amount',projectId:'@projectId'}),
-		projectShop: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectShop', {projectId:'@projectId',amount:'@amount'})
+		projectShop: $resource(DEFAULT_DOMAIN + '/siteProject/getProjectShop', {projectId:'@projectId',amount:'@amount'}),
+		sponsorInstitution: $resource(DEFAULT_DOMAIN + '/siteProject/sponsorInstitution', {guaranteeId:'@guaranteeId'}),
 	}
 });
