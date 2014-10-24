df0d02ff08d5869582bd4dbe10179a9f90de5c1d
dongyao <dongyao@hoolai.com>
2014-10-24 15:11:50 +0800

    细节修改

--- app/scripts/controller/get-pwd-back/get-pwd-back-ctrl.js
+++ app/scripts/controller/get-pwd-back/get-pwd-back-ctrl.js
@@ -5,55 +5,35 @@ hongcaiApp
   	$scope.refreshCode = function() {
         angular.element("#checkCaptcha").attr("src", angular.element("#checkCaptcha").attr("src").substr(0, angular.element("#checkCaptcha").attr("src").indexOf('?')) + "?code=" + Math.random());
     };
-    
     $scope.verifyAccount = function(){
-      var dataBoth=[
-        {"CategoryId":0, "Name":"手机找回" },
-        {"CategoryId":1, "Name":"邮箱找回"}
-      ] ;
-      var dataPhone=[
-        {"CategoryId":0, "Name":"手机找回" }
-      ] ;
-      var dataEmail=[
-        {"CategoryId":1, "Name":"邮箱找回"}
-      ] ;
+      var dataBoth=[{"CategoryId":0, "Name":"手机找回" }, {"CategoryId":1, "Name":"邮箱找回"}];
+      var dataPhone=[{"CategoryId":0, "Name":"手机找回"}];
+      var dataEmail=[{"CategoryId":1, "Name":"邮箱找回"}];
       if($scope.usermessage.mobile && $scope.usermessage.email){
-        $scope.Category1= dataBoth;
-        $scope.wayGetFlag = 1;
-        $scope.findWay = '手机';
-        $scope.tipsFlag = 1;
-        $scope.wayBack = '手机找回';
+        $scope.Category = dataBoth;
       }else if($scope.usermessage.mobile){
-        $scope.Category1= dataPhone;
-        $scope.wayGetFlag = 1;
-        $scope.findWay = '手机';
-        $scope.tipsFlag = 1;
-        $scope.wayBack = '手机找回';
+      $scope.Category = dataPhone;
       }else if($scope.usermessage.email){
-        $scope.Category1= dataEmail;
-        $scope.wayGetFlag = 2;
-        $scope.findWay = '邮箱';
-        $scope.tipsFlag = 2;
-        $scope.wayBack = '邮箱找回';
+        $scope.Category = dataEmail;
       }
-      $scope.$watch('Category1Val', function (newValue) {
-        if($scope.CategoryId == 1){
-          console.log("1");
-          $scope.wayGetFlag = 2;
-        $scope.findWay = '邮箱';
-        $scope.tipsFlag = 2;
-        $scope.wayBack = '邮箱找回';
+      $scope.$watch('CategoryVal', function (CategoryId) {
+        if(CategoryId != 0) {
+          $scope.isDisplay = false;
         }else{
-          console.log("0");
-          $scope.wayGetFlag = 1;
-        $scope.findWay = '手机';
-        $scope.tipsFlag = 1;
-        $scope.wayBack = '手机找回';
+          $scope.isDisplay = true;
         }
-        //$scope.Category2Val = $scope.Category1.Product[0];//對應組的第一個
-      })
+      });
       $scope.areaFlag = 2;
     }
+    $scope.sendMobileCaptcha = function() {
+        RegisterService.sendMobileCaptcha.save({mobile: angular.element("#mobile").val() }, function(response) {
+            if(response.ret == 1) {
+                
+            } else {
+                
+            }
+        });
+    };
 }])
 .controller("SetPwdCtrl", ["$scope", "$state", "$rootScope", "$stateParams", "SessionService", "DEFAULT_DOMAIN", "toaster","GetPwdService", function ($scope, $state, $rootScope, $stateParams, SessionService, DEFAULT_DOMAIN, toaster,GetPwdService) {
     $scope.areaFlag = 3;
