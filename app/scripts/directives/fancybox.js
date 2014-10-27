74825959a3d205110a3f41a8579ad8366909fe45
leopku <leopku@qq.com>
2014-10-27 11:26:05 +0800

    fancybox

---
+++ app/scripts/directives/fancybox.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name p2pSiteWebApp.directive:fancybox
+ * @description
+ * # fancybox
+ */
+angular.module('hongcaiApp')
+  .directive('fancybox', function ($window) {
+    return {
+      // template: '<div></div>',
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element.bind('click', function () {
+          var url = attrs.hcPreviewUrl;
+          // var opt = attra.hcOption;
+          $window.$.fancybox.open(url);
+          // angular.element('img[fancybox]').fancybox();
+        });
+      }
+    };
+  });
