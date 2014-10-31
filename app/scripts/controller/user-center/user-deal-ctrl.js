5472061af7fc579fe300abcc9462d9ccb67ddaf7
chenhui <chenhui@hoolai.com>
2014-10-31 18:19:40 +0800

    record

--- app/scripts/controller/user-center/user-deal-ctrl.js
+++ app/scripts/controller/user-center/user-deal-ctrl.js
@@ -1,14 +1,28 @@
 hongcaiApp.controller("UserDealCtrl", ["$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", function ($scope,$rootScope, $state, $stateParams, UserCenterService) {
     
+    var dateStart = 0;
+	var dateEnd = 0;
+
+	$scope.fromDealDateChanged = function () {
+        dateStart = $scope.fromDate;
+
+    };
+
+    $scope.untilDealDateChanged = function (status,dateInterval) {
+        $location.path('userCenter-investment/:'+dateInterval+'/:'+status+'/:'+dateStart+'/:'+dateEnd)
+
+    };
+
     var getDealByUser = UserCenterService.getDealByUser.get({ dateInterval: $stateParams.dateInterval,
     															type: $stateParams.type},
     															function(response) {
         $scope.dealList = getDealByUser.data.dealList;
-        $scope.startTime = getDealByUser.data.startTime;
-        $scope.endTime = getDealByUser.data.endTime;
+        $scope.dateStart = getOrderByUser.data.dateStart;
+        $scope.dateEnd = getOrderByUser.data.dateEnd;
         $scope.type = getDealByUser.data.type;
         $scope.dateInterval = getDealByUser.data.dateInterval;
         $scope.userId = getDealByUser.data.userId;
+        $scope.capital = getDealByUser.data.capital;
 
     });
 }]);
