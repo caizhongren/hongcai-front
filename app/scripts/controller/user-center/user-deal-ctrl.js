97662e6994db7155442ef19661d1de51d59cca2b
chenhui <chenhui@hoolai.com>
2014-10-31 10:50:49 +0800

    record bill

--- app/scripts/controller/user-center/user-deal-ctrl.js
+++ app/scripts/controller/user-center/user-deal-ctrl.js
@@ -1,12 +1,9 @@
 hongcaiApp.controller("UserDealCtrl", ["$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", function ($scope,$rootScope, $state, $stateParams, UserCenterService) {
     
-	$rootScope.selectSide = 'userCenter-investment';
     var getDealByUser = UserCenterService.getDealByUser.get({ dateInterval: $stateParams.dateInterval,
     															type: $stateParams.type},
     															function(response) {
         $scope.dealList = getDealByUser.data.dealList;
-		$scope.dateInterval = getOrderByUser.data.dateInterval;
-        $scope.type = getOrderByUser.data.type;
 
     });
 }]);
