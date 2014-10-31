ed687ccae860132f2e7b8508fbb980b8bce3adb3
dongyao <dongyao@hoolai.com>
2014-10-31 17:18:35 +0800

    个人中心细节优化

--- app/scripts/controller/user-center/user-center-ctrl.js
+++ app/scripts/controller/user-center/user-center-ctrl.js
@@ -31,6 +31,20 @@ hongcaiApp.controller("UserCenterCtrl", [ "$location", "$scope", "$state", "$roo
     $rootScope.isSecurityAuth = false;
 
     /***************************** sidebar start *************************/
+    $scope.timestamp = new Date();
+    var welcomeTime = $scope.timestamp.getHours();
+    if(welcomeTime > 5 && welcomeTime < 9){
+        $scope.welcomeTip = '早安~';
+    }else if(welcomeTime >= 9 && welcomeTime <= 11){
+        $scope.welcomeTip = '上午好，';
+    }else if(welcomeTime >= 12 && welcomeTime <= 14){
+        $scope.welcomeTip = '中午好，';
+    }else if(welcomeTime >= 15 && welcomeTime <= 18){
+        $scope.welcomeTip = '下午好，';
+    }else {
+        $scope.welcomeTip = '晚安~';
+    }
+
     $rootScope.selectSide = $location.path().substr($location.path().indexOf("/") + 1);
     UserCenterService.userSecurityInfo.get({}, function(response) {
         if(response.ret == 1) {
