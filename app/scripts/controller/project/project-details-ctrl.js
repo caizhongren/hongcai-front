10fb8e4c15d624986f9a861a48d9b582b52005b5
chenhui <chenhui@hoolai.com>
2014-11-13 17:45:03 +0800

    project detail

--- app/scripts/controller/project/project-details-ctrl.js
+++ app/scripts/controller/project/project-details-ctrl.js
@@ -5,10 +5,11 @@ hongcaiApp.controller("ProjectDetailsCtrl", ["$scope", "$state", "$rootScope", "
     var projectDetails = ProjectService.projectDetails.get({projectId: $stateParams.projectId}, function() {
         $scope.project = projectDetails.data.project;
         $scope.projectInfo = projectDetails.data.projectInfo;
-        $scope.pledges = projectDetails.data.pledges;
+        // $scope.pledges = projectDetails.data.pledges;
         $scope.isAvailable = projectDetails.data.isAvailable;
         $scope.enterprise = projectDetails.data.enterprise;
         $scope.orderList = projectDetails.data.orderList;
+        $scope.enterpriseFile = projectDetails.data.enterpriseFile;
         
 
         // var project = projectDetails.data.project;
