364c8d10529b1756b6f8b300a3953198d9e261b4
wangkaijie <wangkaijie@hoolai.com>
2014-11-18 18:15:42 +0800

    处理个人中心-支付记录，投资产品。

--- app/scripts/controller/user-center/user-deal-ctrl.js
+++ app/scripts/controller/user-center/user-deal-ctrl.js
@@ -1,41 +1,45 @@
-hongcaiApp.controller("UserDealCtrl", ["$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", function ($scope,$rootScope, $state, $stateParams, UserCenterService) {
+hongcaiApp.controller("UserDealCtrl", ["$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", "$location", function ($scope,$rootScope, $state, $stateParams, UserCenterService, $location) {
 
   $rootScope.selectSide = 'record';
   var dateStart = 0;
   var dateEnd = 0;
+  $scope.type = $stateParams.type;
+  $scope.dateInterval = $stateParams.dateInterval
 
   $scope.fromDealDateChanged = function () {
     dateStart = $scope.fromDate;
-    $location.path('record/:'+dateInterval+'/:'+status+'/:'+dateStart+'/:'+dateEnd);
+    $location.path('record/'+$scope.dateInterval+'/'+$scope.status+'/'+dateStart+'/'+dateEnd);
 
     };
 
-    $scope.untilDealDateChanged = function (status,dateInterval) {
-        dateEnd = $scope.endDate;
-        $location.path('record/:'+dateInterval+'/:'+status+'/:'+dateStart+'/:'+dateEnd);
-    };
+  $scope.untilDealDateChanged = function (status,dateInterval) {
+    dateEnd = $scope.endDate;
+    $location.path('record/'+$scope.dateInterval+'/'+$scope.status+'/'+dateStart+'/'+dateEnd);
+  };
 
-    var getDealByUser = UserCenterService.getDealByUser.get({ dateInterval: $stateParams.dateInterval, type: $stateParams.type},function(response) {
+  var getDealByUser = UserCenterService.getDealByUser.get({ dateInterval: $stateParams.dateInterval, type: $stateParams.type},function(response) {
 
-        $scope.dealList = getDealByUser.data.dealList;
-        $scope.dateStart = getDealByUser.data.dateStart;
-        $scope.dateEnd = getDealByUser.data.dateEnd;
-        $scope.type = getDealByUser.data.type;
-        $scope.dateInterval = getDealByUser.data.dateInterval;
-        $scope.userId = getDealByUser.data.userId;
-        $scope.capital = getDealByUser.data.capital;
-        $scope.currentPage = 0;
-        $scope.pageSize = 10;
-        $scope.data = [];
-        console.info(getDealByUser.data.dealList);
-        $scope.numberOfPages = function() {
-            return Math.ceil($scope.data.length / $scope.pageSize);
-        }
-        for (var i = 0; i < $scope.dealList.length; i++) {
-            $scope.data.push($scope.dealList[i]);
-        }
+    $scope.dealList = getDealByUser.data.dealList;
+    $scope.dateStart = getDealByUser.data.dateStart;
+    $scope.dateEnd = getDealByUser.data.dateEnd;
+    $scope.type = getDealByUser.data.type;
+    $scope.dateInterval = getDealByUser.data.dateInterval;
+    $scope.userId = getDealByUser.data.userId;
+    $scope.capital = getDealByUser.data.capital;
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.data = [];
+    console.info(getDealByUser.data.dealList);
+    $scope.numberOfPages = function() {
+        return Math.ceil($scope.data.length / $scope.pageSize);
+    }
+    for (var i = 0; i < $scope.dealList.length; i++) {
+        $scope.data.push($scope.dealList[i]);
 
-    });
+    }
+    console.log($scope.data)
+
+  });
 
 }]);
 
--- app/scripts/controller/user-center/user-order-ctrl.js
+++ app/scripts/controller/user-center/user-order-ctrl.js
@@ -1,23 +1,25 @@
-hongcaiApp.controller("UserOrderCtrl", ["$location", "$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", function ($location,$scope,$rootScope, $state, $stateParams, UserCenterService) {
+hongcaiApp.controller("UserOrderCtrl", ["$location", "$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", "$location", function ($location,$scope,$rootScope, $state, $stateParams, UserCenterService, $location) {
 
     $rootScope.selectSide = 'userCenter-investment';
 
     var dateStart = 0;
     var dateEnd = 0;
+    $scope.status = $stateParams.status || 0;
+    $scope.dateInterval = $stateParams.dateInterval || 0;
 
     $scope.fromDateChanged = function () {
       dateStart = $scope.fromDate;
-      $location.path($rootScope.selectSide+'/:'+dateInterval+'/:'+status+'/:'+dateStart+'/:'+dateEnd);
+      $location.path("userCenter-investment"+'/'+$scope.dateInterval+'/'+$scope.status+'/'+dateStart+'/'+dateEnd);
     };
 
     $scope.untilDealDateChanged = function (status,dateInterval) {
       dateEnd = $scope.endDate;
-      $location.path($rootScope.selectSide+'/:'+dateInterval+'/:'+status+'/:'+dateStart+'/:'+dateEnd)
+      $location.path("userCenter-investment"+'/'+$scope.dateInterval+'/'+$scope.status+'/'+dateStart+'/'+dateEnd);
     };
 
     var getOrderByUser = UserCenterService.getOrderByUser.get({ dateInterval: $stateParams.dateInterval,
-    															status: $stateParams.status},
-    															function(response) {
+                                                              status: $stateParams.status},
+                                                              function(response) {
         $scope.orderList = getOrderByUser.data.orderVoList;
         $scope.orderCount = getOrderByUser.data.orderCount;
         $scope.amount = getOrderByUser.data.amount;
