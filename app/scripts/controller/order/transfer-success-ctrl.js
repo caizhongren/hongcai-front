993e3e07db1af2c716c022ade9eb4a38fa6f1e8d
zhangfeng <zhangfeng@hoolai.com>
2014-11-22 04:50:49 +0800

    成功页面提醒

--- app/scripts/controller/order/transfer-success-ctrl.js
+++ app/scripts/controller/order/transfer-success-ctrl.js
@@ -1,16 +1,7 @@
 hongcaiApp.controller('TransferSuccessCtrl', ["$scope", "$state", "$rootScope", "$stateParams", "RegisterService", "SessionService", "DEFAULT_DOMAIN", "toaster", function ($scope, $state, $rootScope, $stateParams, RegisterService, SessionService, DEFAULT_DOMAIN, toaster) {
 
 
-    $scope.message = '注册成功';
-    var etoken = $stateParams.etoken;
-    if(etoken){
-        RegisterService.activeEmail.get({etoken: etoken}, function(response){
-            if (response.ret == -1){
-                $scope.message = response.msg;
-            }
-
-        });
-    };
+    $scope.page='yeepay-transfer';
 
 }]);
  
