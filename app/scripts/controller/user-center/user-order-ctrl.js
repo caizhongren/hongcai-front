68acc6d2ea6312d8fa3901004908e361e5da660f
chenhui <chenhui@hoolai.com>
2014-11-22 07:41:16 +0800

    gift order

--- app/scripts/controller/user-center/user-order-ctrl.js
+++ app/scripts/controller/user-center/user-order-ctrl.js
@@ -1,5 +1,6 @@
 hongcaiApp.controller("UserOrderCtrl", ["$location", "$scope", "$rootScope", "$state", "$stateParams", "UserCenterService", function ($location,$scope,$rootScope, $state, $stateParams, UserCenterService) {
 
+    $rootScope.redirectUrl = $location.path();
     $rootScope.selectSide = 'userCenter-investment';
 
     var dateStart = 0;
@@ -30,8 +31,8 @@ hongcaiApp.controller("UserOrderCtrl", ["$location", "$scope", "$rootScope", "$s
       $location.path('userCenter-investment/'+dateInterval+'/'+status+'/'+dateStart+'/'+dateEnd)
     };
 
-    var getOrderByUser = UserCenterService.getOrderByUser.get({ dateInterval: $stateParams.dateInterval,
-    															status: $stateParams.status,dateStart: $stateParams.dateStart,dateEnd: $stateParams.dateEnd,type: 1},
+    var getOrderByUser = UserCenterService.getOrderByUser.get({type: $stateParams.type, dateInterval: $stateParams.dateInterval,
+    															status: $stateParams.status,dateStart: $stateParams.dateStart,dateEnd: $stateParams.dateEnd},
     															function(response) {
 
         $scope.orderList = getOrderByUser.data.orderVoList;
