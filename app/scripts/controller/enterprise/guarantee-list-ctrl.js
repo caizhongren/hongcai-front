90051ad091fe412a51407d852c0efc6164672763
crystal <gisele6@126.com>
2014-11-25 17:13:34 +0800

    sponsor

--- app/scripts/controller/enterprise/guarantee-list-ctrl.js
+++ app/scripts/controller/enterprise/guarantee-list-ctrl.js
@@ -3,62 +3,47 @@ hongcaiApp.controller('GuaranteeListCtrl', ['$scope', '$stateParams', '$location
 
     var sponsor = GuaranteeService.guaranteeList.get(function() {
         $scope.guaranteeList = sponsor.data.guaranteeList;
+        console.log($scope.guaranteeList)
     });
 
-    $scope.imgs = [
-        {
-            title:'百度',
-            src:'images/test/0.png'
-        },
-        {
-            title:'腾讯',
-            src:'images/test/1.png'
-        },
-        {
-            title:'搜狐',
-            src:'images/test/2.png'
-        },
-        {
-            title:'网易',
-            src:'images/test/3.png'
-        },
-        {
-            title:'优酷',
-            src:'images/test/4.png'
-        },
-        {
-            title:'土豆',
-            src:'images/test/5.png'
-        },
-        {
-            title:'雅虎',
-            src:'images/test/6.png'
-        },
-        {
-            title:'网易',
-            src:'images/test/7.png'
-        },
-        {
-            title:'优酷',
-            src:'images/test/8.png'
-        },
-        {
-            title:'土豆',
-            src:'images/test/9.png'
-        },
-        {
-            title:'雅虎',
-            src:'images/test/10.png'
+    $scope.media = [
+        {'items':
+            [
+                {src:'images/sponsor.png', href:'/',name:'天天担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天nht担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天hth担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天nht担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天hth担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天nht担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天hth担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天nht担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'天hth担保',registerCapital:'500',id:'12'}
+            ]
         }
+        ,{'items':
+            [
+                {src:'images/sponsor.png', href:'/',name:'hgjj担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:' rdhtt担保',registerCapital:'500',id:'12'},
+                {src:'images/sponsor.png', href:'/',name:'gfh担保',registerCapital:'500',id:'12'}
+            ]
+        }
+       
+        // {src:'images/banner-3.png', href:'' }
     ];
 
-    $(function(){
-        $('#slider').sudySlider($scope.imgs);
-    });
+    $scope.slickConfig = {
+        dots: true,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        onAfterChange: function(slick, index) {
+            var slides = $('.slick-track').children().not('.slick-cloned');
+            if (index >= slides.length) return;
+            // $(slides[index]).find('video').each(playVideo);
+        }
+    };
 
-    // if($(window).scrollTop()>100){
-    //     $('body,html').animate({scrollTop:0},800);
-    // }
+    $scope.slickHandle = {};
+
 
 }]);
 
