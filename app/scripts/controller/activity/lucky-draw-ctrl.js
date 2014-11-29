b97fdd750cb64cfe575dd841ad51a313ffb4e106
wangkaijie <wangkaijie@hoolai.com>
2014-11-29 18:47:50 +0800

    抽奖页(CSS还待调整)

--- app/scripts/controller/activity/lucky-draw-ctrl.js
+++ app/scripts/controller/activity/lucky-draw-ctrl.js
@@ -1,3 +1,7 @@
-hongcaiApp.controller('LuckyDrawCtrl', ['$scope', '$window', function ($scope, $window) {
-
+hongcaiApp.controller('LuckyDrawCtrl', ['$scope', '$window', 'UserCenterService', function ($scope, $window, UserCenterService ) {
+  $scope.luckyDraw = UserCenterService.luckyDraw.get(function(response){
+    if(response === 1) {
+      $scope.status = 1;
+    }
+  })
 }]);
