16a57dce9348641c2fa13f12bce0df0658bf2dd0
crystal <gisele6@126.com>
2014-12-01 17:52:40 +0800

    sponsorInstitution 图片绑定

--- app/scripts/controller/project/project-sponsorInstitution-ctrl.js
+++ app/scripts/controller/project/project-sponsorInstitution-ctrl.js
@@ -5,99 +5,22 @@ hongcaiApp.controller('ProjectSponsorInstitutionCtrl', ['$scope', '$stateParams'
         $scope.projectList = sponsorInstitution.data.projectList;
         $scope.guarantee = sponsorInstitution.data.guarantee;
         $scope.guaranteeProjectVo = sponsorInstitution.data.guaranteeProjectVo;
-    });
+        $scope.originalFile = sponsorInstitution.data.originalFile;
 
-
-
-
-    /*var projectList = ProjectService.projectList.get({status: $stateParams.status,
-    												  minCycle: $stateParams.minCycle,
-    												  maxCycle: $stateParams.maxCycle,
-    												  minEarning: $stateParams.minEarning,
-    												  maxEarning: $stateParams.maxEarning,
-    											  minTotalAmount: $stateParams.minTotalAmount,
-    												  maxTotalAmount: $stateParams.maxTotalAmount,
-    												  sortCondition: $stateParams.sortCondition,
-    												  sortType: $scope.sortType}, function() {
-        $scope.status = $stateParams.status;
-        $scope.minCycle = $stateParams.minCycle;
-        $scope.maxCycle = $stateParams.maxCycle;
-        $scope.minEarning = $stateParams.minEarning;
-        $scope.maxEarning = $stateParams.maxEarning;
-        $scope.minTotalAmount = $stateParams.minTotalAmount;
-        $scope.maxTotalAmount = $stateParams.maxTotalAmount;
-        $scope.sortCondition = $stateParams.sortCondition;
-        $scope.orderProp = 'id';
-        $scope.currentPage = 0;
-        $scope.pageSize = 15;
-        $scope.data = [];
-		$scope.projectList = projectList.data;
-
-	    $scope.numberOfPages = function() {
-	        return Math.ceil($scope.data.length / $scope.pageSize);
-	    }
-	    for (var i = 0; i < $scope.projectList.projectList.length; i++) {
-	        $scope.data.push($scope.projectList.projectList[i]);
-	    }
-
-	    $scope.sortType = false;
-	    $scope.toggleSort = function() {
-	    	$scope.sortType = !$scope.sortType;
-    	};
-	});*/
-    // $scope.image = 'images/test/0.png';
-    $scope.imgs = [
-        {
-            title:'百度',
-            src:'images/test/0.png'
-        },
-        {
-            title:'腾讯',
-            src:'images/test/1.png'
-        },
-        {
-            title:'搜狐',
-            src:'images/test/2.png'
-        },
-        {
-            title:'网易',
-            src:'images/test/3.png'
-        },
-        {
-            title:'优酷',
-            src:'images/test/4.png'
-        },
-        {
-            title:'土豆',
-            src:'images/test/5.png'
-        },
-        {
-            title:'雅虎',
-            src:'images/test/6.png'
-        },
-        {
-            title:'网易',
-            src:'images/test/7.png'
-        },
-        {
-            title:'优酷',
-            src:'images/test/8.png'
-        },
-        {
-            title:'土豆',
-            src:'images/test/9.png'
-        },
-        {
-            title:'雅虎',
-            src:'images/test/10.png'
+        $scope.imgs = [];
+        for(i in sponsorInstitution.data.originalFile){
+            var item = {};
+            item.title = $scope.originalFile[i].uploadFile.originalName;
+            item.src = $scope.originalFile[i].uploadFile.url;
+            $scope.imgs.push(item);
         }
-    ];
 
-    $(function(){
-        $('#slider').sudySlider($scope.imgs);
+        $(function(){
+            $('#slider').sudySlider($scope.imgs);
+        });
+
     });
-
-
+  
 }]);
 
 
