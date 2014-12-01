dd7b027678c7f6a8686d98e361f1d18d34bbb262
wangkaijie <wangkaijie@hoolai.com>
2014-12-01 17:49:25 +0800

    抽奖活动，js实现部分。

--- app/scripts/controller/activity/lucky-draw-ctrl.js
+++ app/scripts/controller/activity/lucky-draw-ctrl.js
@@ -1,7 +1,19 @@
 hongcaiApp.controller('LuckyDrawCtrl', ['$scope', '$window', 'UserCenterService', function ($scope, $window, UserCenterService ) {
-  $scope.luckyDraw = UserCenterService.luckyDraw.get(function(response){
-    if(response === 1) {
-      $scope.status = 1;
-    }
-  })
+  $scope.status = 0;
+  $scope.luckyDraw = function() {
+    UserCenterService.luckyDraw.get(function(response){
+      if(response.ret === 1) {
+        var hongbaoLevel = response.data.userHongbaoLottery.prizeLevel;
+        if(hongbaoLevel === 0) {
+          // 土豪状态
+          $scope.status = 2;
+        } else if (hongbaoLevel === 1) {
+          // 普通宏包状态。
+          $scope.status = 1;
+        }
+      } else {
+        $window.alert(response.msg);
+      }
+    })
+  };
 }]);
