cbf20aa093a8450f5e621b501d463794c7479a46
crystal <gisele6@126.com>
2014-12-02 13:12:00 +0800

    保荐机构详情页

--- app/scripts/controller/project/project-sponsorInstitution-ctrl.js
+++ app/scripts/controller/project/project-sponsorInstitution-ctrl.js
@@ -7,7 +7,7 @@ hongcaiApp.controller('ProjectSponsorInstitutionCtrl', ['$scope', '$stateParams'
         $scope.guaranteeProjectVo = sponsorInstitution.data.guaranteeProjectVo;
         $scope.originalFile = sponsorInstitution.data.originalFile;
 
-        $scope.imgs = [];
+        /*$scope.imgs = [];
         for(var i=0; i<$scope.originalFile.length;i++){
             var item = {};
             item.title = $scope.originalFile[i].uploadFile.originalName;
@@ -15,9 +15,31 @@ hongcaiApp.controller('ProjectSponsorInstitutionCtrl', ['$scope', '$stateParams'
             $scope.imgs.push(item);
         }
 
+        console.log($scope.imgs)
+
         $(function(){
             $('#slider').sudySlider($scope.imgs);
-        });
+        });*/
+
+        $scope.media = [
+          {mimeType: 'image/png', src:'images/banner-1.png', href:'' },
+          {mimeType: 'image/png', src:'images/banner-2.png', href:'' },
+          {mimeType: 'image/png', src:'images/banner-3.png', href:'banner-nine.html'},
+          {mimeType: 'image/png', src:'images/banner-4.png', href:'' },
+          {mimeType: 'image/png', src:'images/banner-5.png', href:'' }
+        ];
+        $scope.slickConfig = {
+          dots: true,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          onAfterChange: function(slick, index) {
+              var slides = $('.slick-track').children().not('.slick-cloned');
+              if (index >= slides.length) return;
+              // $(slides[index]).find('video').each(playVideo);
+          }
+        };
+        $scope.slickHandle = {
+        };
 
     });
   
