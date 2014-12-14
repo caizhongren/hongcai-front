edc740f5e296cde1e77c16bcd58c475601061e46
wangkaijie <wangkaijie@hoolai.com>
2014-12-14 18:00:51 +0800

    处理grunt jshint 的警告的内容。

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -875,21 +875,7 @@ hongcaiApp.run(function($rootScope, $location, $window, $http, DEFAULT_DOMAIN) {
         if (this[i] === obj) { return i; }
      }
      return -1;
-    }
-    // Array.prototype.indexOf = function(elt /*, from*/) {
-    //   var len = this.length >>> 0;
-    //   var from = Number(arguments[1]) || 0;
-    //   from = (from < 0) ? Math.ceil(from) : Math.floor(from);
-    //   if (from < 0) {
-    //     from += len;
-    //   }
-    //   for (; from < len; from++) {
-    //     if (from in this && this[from] === elt) {
-    //       return from;
-    //     }
-    //   }
-    //   return -1;
-    // };
+    };
   }
 
   var routespermission = ['/account-overview',
--- app/scripts/directives/ieselectfix.js
+++ app/scripts/directives/ieselectfix.js
@@ -11,18 +11,20 @@ angular.module('hongcaiApp')
     return {
       restrict: 'A',
       require: 'ngModel',
-      link: function(scope, element, attributes, ngModelCtrl) {
+      link: function(scope, element, attributes) {
         var isIE = $document[0] && $document[0].attachEvent;
-        if (!isIE) return;
+        if (!isIE) {
+          return;
+        }
         var control = element[0];
         scope.$watch(attributes.ieSelectFix, function() {
           setTimeout(function() {
-            var option = document.createElement("option");
+            var option = document.createElement('option');
             control.add(option,null);
             control.remove(control.options.length-1);
           }, 0);
         });
       }
-    }
+    };
   }
 ]);
