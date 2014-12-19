eb3efedf4e9b86545850568f3309de9bbc69eba2
gaodi <gaodi@hoolai.com>
2014-12-19 17:04:08 +0800

    修改手机注册成功后5秒自动跳转功能，注册成功页面细节优化

--- app/scripts/controller/register/register-mobile-success-ctrl.js
+++ app/scripts/controller/register/register-mobile-success-ctrl.js
@@ -2,15 +2,15 @@
 hongcaiApp.controller('RegisterMobileSuccessCtrl', ['$scope', '$timeout', '$state', '$rootScope', '$stateParams', 'RegisterService', 'SessionService', 'DEFAULT_DOMAIN', 'toaster', '$location', function ($scope, $timeout, $state, $rootScope, $stateParams, RegisterService, SessionService, DEFAULT_DOMAIN, toaster, $location) {
     $scope.page = 3;
     $scope.counter = 5;
-    $scope.onTimeout = function(){
-      $scope.counter--;
-      mytimeout = $timeout($scope.onTimeout,1000);
-      if($scope.counter === 0) {
-        $state.go('root.userCenter.security-settings');
-      }
-    }
-    var mytimeout = $timeout($scope.onTimeout,1000);
-    $scope.$on('$stateChangeStart', function(){
-      $timeout.cancel(mytimeout);
-    });
+    // $scope.onTimeout = function(){
+    //   $scope.counter--;
+    //   mytimeout = $timeout($scope.onTimeout,1000);
+    //   if($scope.counter === 0) {
+    //     $state.go('root.userCenter.security-settings');
+    //   }
+    // }
+    // var mytimeout = $timeout($scope.onTimeout,1000);
+    // $scope.$on('$stateChangeStart', function(){
+    //   $timeout.cancel(mytimeout);
+    // });
 }]);
