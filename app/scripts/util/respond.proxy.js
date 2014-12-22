a9afea08f95f66d93836e0a6fe23dad422c3c46d
wangkaijie <wangkaijie@hoolai.com>
2014-12-22 15:23:12 +0800

    支持IE8的JS做了基本的调整。'use strict';

--- app/scripts/util/respond.proxy.js
+++ app/scripts/util/respond.proxy.js
@@ -1,11 +1,10 @@
+'use strict';
 /*! Respond.js: min/max-width media query polyfill. Remote proxy (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
 (function(win, doc, undefined){
 	var docElem			= doc.documentElement,
-		proxyURL		= doc.getElementById("respond-proxy").href,
-		redirectURL		= (doc.getElementById("respond-redirect") || location).href,
-		baseElem		= doc.getElementsByTagName("base")[0],
-		urls			= [],
-		refNode;
+		proxyURL		= doc.getElementById('respond-proxy').href,
+		redirectURL		= (doc.getElementById('respond-redirect') || location).href,
+		baseElem		= doc.getElementsByTagName('base')[0];
 
 	function encode(url){
 		return win.encodeURIComponent(url);
@@ -15,23 +14,23 @@
 
 		var iframe,
 			AXO;
-		
+
 		// All hail Google http://j.mp/iKMI19
 		// Behold, an iframe proxy without annoying clicky noises.
-		if ( "ActiveXObject" in win ) {
-			AXO = new ActiveXObject( "htmlfile" );
+		if ( 'ActiveXObject' in win ) {
+			AXO = new ActiveXObject( 'htmlfile' );
 			AXO.open();
 			AXO.write( '<iframe id="x"></iframe>' );
 			AXO.close();
-			iframe = AXO.getElementById( "x" );
+			iframe = AXO.getElementById( 'x' );
 		} else {
-			iframe = doc.createElement( "iframe" );
-			iframe.style.cssText = "position:absolute;top:-99em";
+			iframe = doc.createElement( 'iframe' );
+			iframe.style.cssText = 'position:absolute;top:-99em';
 			docElem.insertBefore(iframe, docElem.firstElementChild || docElem.firstChild );
 		}
 
-		iframe.src = checkBaseURL(proxyURL) + "?url=" + encode(redirectURL) + "&css=" + encode(checkBaseURL(url));
-		
+		iframe.src = checkBaseURL(proxyURL) + '?url=' + encode(redirectURL) + '&css=' + encode(checkBaseURL(url));
+
 		function checkFrameName() {
 			var cssText;
 
@@ -42,11 +41,11 @@
 
 			if (cssText) {
 				// We've got what we need. Stop the iframe from loading further content.
-				iframe.src = "about:blank";
+				iframe.src = 'about:blank';
 				iframe.parentNode.removeChild(iframe);
 				iframe = null;
 
-			
+
 				// Per http://j.mp/kn9EPh, not taking any chances. Flushing the ActiveXObject
 				if (AXO) {
 					AXO = null;
@@ -62,7 +61,7 @@
 				win.setTimeout(checkFrameName, 100);
 			}
 		}
-		
+
 		win.setTimeout(checkFrameName, 500);
 	}
 
@@ -76,14 +75,14 @@
         el.innerHTML = '<a href="' + escapedURL + '">x</a>';
         return el.firstChild.href;
 	}
-	
+
 	function checkRedirectURL() {
 		// IE6 & IE7 don't build out absolute urls in <link /> attributes.
 		// So respond.proxy.gif remains relative instead of http://example.com/respond.proxy.gif.
 		// This trickery resolves that issue.
 		if (~ !redirectURL.indexOf(location.host)) {
 
-			var fakeLink = doc.createElement("div");
+			var fakeLink = doc.createElement('div');
 
 			fakeLink.innerHTML = '<a href="' + redirectURL + '"></a>';
 			docElem.insertBefore(fakeLink, docElem.firstElementChild || docElem.firstChild );
@@ -96,31 +95,31 @@
 			fakeLink = null;
 		}
 	}
-	
+
 	function buildUrls(){
-		var links = doc.getElementsByTagName( "link" );
-		
+		var links = doc.getElementsByTagName( 'link' );
+
 		for( var i = 0, linkl = links.length; i < linkl; i++ ){
-			
+
 			var thislink	= links[i],
 				href		= links[i].href,
 				extreg		= (/^([a-zA-Z:]*\/\/(www\.)?)/).test( href ),
 				ext			= (baseElem && !extreg) || extreg;
 
 			//make sure it's an external stylesheet
-			if( thislink.rel.indexOf( "stylesheet" ) >= 0 && ext ){
-				(function( link ){			
+			if( thislink.rel.indexOf( 'stylesheet' ) >= 0 && ext ){
+				(function( link ){
 					fakejax( href, function( css ){
 						link.styleSheet.rawCssText = css;
 						respond.update();
 					} );
 				})( thislink );
-			}	
+			}
 		}
 
-		
+
 	}
-	
+
 	if( !respond.mediaQueriesSupported ){
 		checkRedirectURL();
 		buildUrls();
