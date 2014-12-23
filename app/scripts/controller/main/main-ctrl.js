0014a9ded7cd8685d92c974f57f6ef7a242b700c
wangkaijie <wangkaijie@hoolai.com>
2014-12-23 16:15:30 +0800

    主页面两次刷新代码清除(banner的调用放到sider-ctrl)

--- app/scripts/controller/main/main-ctrl.js
+++ app/scripts/controller/main/main-ctrl.js
@@ -118,28 +118,6 @@ hongcaiApp.controller('MainCtrl', ['$scope', '$stateParams', '$rootScope', '$loc
 
     $rootScope.selectPage = $location.path().split('/')[1];
 
-
-    $scope.media = [
-      // {mimeType: 'image/png', src:'images/banner-1.png', href:'/project-sponsorInstitution/140' },
-      // {mimeType: 'image/png', src:'images/banner-2.png', href:'/banner-fourty' },
-      {mimeType: 'image/png', src:'images/banner-6.png', href:'/banner-P2B' },
-      {mimeType: 'image/png', src:'images/banner-3.png', href:'/banner-nine'},
-      {mimeType: 'image/png', src:'images/banner-4.png', href:'/project-activity-group' },
-      {mimeType: 'image/png', src:'images/banner-5.png', href:'banner-partner' }
-    ];
-    $scope.slickConfig = {
-      dots: true,
-      autoplay: true,
-      autoplaySpeed: 6000,
-      onAfterChange: function(slick, index) {
-          var slides = $('.slick-track').children().not('.slick-cloned');
-          if (index >= slides.length) return;
-          // $(slides[index]).find('video').each(playVideo);
-      }
-    };
-    $scope.slickHandle = {
-    };
-
     /**
      * 处理推广流量统计
      */
