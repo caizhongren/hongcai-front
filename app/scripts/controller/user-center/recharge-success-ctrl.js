00ca60b7a09f97a19fce31c13a09c8c4f8bab23c
wangkaijie <wangkaijie@hoolai.com>
2014-12-24 14:34:28 +0800

    充值成功成功页面的修改

--- app/scripts/controller/user-center/recharge-success-ctrl.js
+++ app/scripts/controller/user-center/recharge-success-ctrl.js
@@ -1,17 +1,18 @@
 'use strict';
-hongcaiApp.controller('RechargeSuccessCtrl', ['$scope', '$timeout', '$state', '$rootScope', '$stateParams', 'RegisterService', 'SessionService', 'DEFAULT_DOMAIN', 'toaster', function ($scope, $timeout, $state, $rootScope, $stateParams, RegisterService, SessionService, DEFAULT_DOMAIN, toaster) {
+hongcaiApp.controller('RechargeSuccessCtrl', ['$scope', function ($scope) {
     $scope.page = 4;
-    $scope.counter = 5;
-    $scope.onTimeout = function(){
-      $scope.counter--;
-      mytimeout = $timeout($scope.onTimeout,1000);
-      if($scope.counter === 0) {
-        window.location.href = '/account-overview';
-      }
-    }
-    var mytimeout = $timeout($scope.onTimeout,1000);
-    $scope.$on('$stateChangeStart', function(){
-      $timeout.cancel(mytimeout);
-    });
+    console.log('page:' + $scope.page);
+    // $scope.counter = 5;
+    // $scope.onTimeout = function(){
+    //   $scope.counter--;
+    //   mytimeout = $timeout($scope.onTimeout,1000);
+    //   if($scope.counter === 0) {
+    //     window.location.href = '/account-overview';
+    //   }
+    // }
+    // var mytimeout = $timeout($scope.onTimeout,1000);
+    // $scope.$on('$stateChangeStart', function(){
+    //   $timeout.cancel(mytimeout);
+    // });
 }]);
 
