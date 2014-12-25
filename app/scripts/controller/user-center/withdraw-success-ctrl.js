13910f7107424772553b5e82586bb2a5d75adeb8
chenmengqing <chenmengqing@hoolai.com>
2014-12-25 11:14:54 +0800

    7个成功跳转页面修改

--- app/scripts/controller/user-center/withdraw-success-ctrl.js
+++ app/scripts/controller/user-center/withdraw-success-ctrl.js
@@ -2,16 +2,16 @@
 hongcaiApp.controller('WithdrawSuccessCtrl', ['$scope', '$timeout', '$state', '$rootScope', '$stateParams', 'RegisterService', 'SessionService', 'DEFAULT_DOMAIN', 'toaster', function ($scope, $timeout, $state, $rootScope, $stateParams, RegisterService, SessionService, DEFAULT_DOMAIN, toaster) {
     $scope.page = 6;
     $scope.counter = 5;
-    $scope.onTimeout = function(){
-      $scope.counter--;
-      mytimeout = $timeout($scope.onTimeout,1000);
-      if($scope.counter === 0) {
-        window.location.href = '/account-overview';
-      }
-    }
-    var mytimeout = $timeout($scope.onTimeout,1000);
-    $scope.$on('$stateChangeStart', function(){
-      $timeout.cancel(mytimeout);
-    });
+    // $scope.onTimeout = function(){
+    //   $scope.counter--;
+    //   mytimeout = $timeout($scope.onTimeout,1000);
+    //   if($scope.counter === 0) {
+    //     window.location.href = '/account-overview';
+    //   }
+    // }
+    // var mytimeout = $timeout($scope.onTimeout,1000);
+    // $scope.$on('$stateChangeStart', function(){
+    //   $timeout.cancel(mytimeout);
+    // });
 }]);
 
