137b914fa0988eebb04768c5b0a0f7a47a2b03fd
crystal <gisele6@126.com>
2015-01-04 09:46:00 +0800

    手动关闭抽奖活动

--- app/scripts/controller/activity/lucky-draw-ctrl.js
+++ app/scripts/controller/activity/lucky-draw-ctrl.js
@@ -45,26 +45,37 @@ hongcaiApp.controller('LuckyDrawCtrl', ['$scope', '$state', 'UserCenterService',
     }
   });
 
+  var luckyDraw_on = false;
   $scope.luckyDraw = function() {
-    UserCenterService.luckyDraw.get(function(response) {
-      if (response.ret === 1) {
-        var hongbaoLevel = response.data.userHongbaoLottery.prizeLevel;
-        if (hongbaoLevel === 0) {
-          // 土豪状态
-          $scope.status = 2;
-        } else if (hongbaoLevel === 1) {
-          // 普通宏包状态。
-          $scope.status = 1;
+    if(!luckyDraw_on){
+      $scope.msg = "活动已过期";
+      var alertDialog = $alert({
+        scope: $scope,
+        template: 'views/modal/alert-dialog.html',
+        show: true
+      });
+    }else {
+      UserCenterService.luckyDraw.get(function(response) {
+        if (response.ret === 1) {
+          var hongbaoLevel = response.data.userHongbaoLottery.prizeLevel;
+          if (hongbaoLevel === 0) {
+            // 土豪状态
+            $scope.status = 2;
+          } else if (hongbaoLevel === 1) {
+            // 普通宏包状态。
+            $scope.status = 1;
+          }
+        } else {
+          $scope.msg = response.msg;
+          var alertDialog = $alert({
+            scope: $scope,
+            template: 'views/modal/alert-dialog.html',
+            show: true
+          });
         }
-      } else {
-        $scope.msg = response.msg;
-        var alertDialog = $alert({
-          scope: $scope,
-          template: 'views/modal/alert-dialog.html',
-          show: true
-        });
-      }
-    })
+      })
+    }
+    
   };
 
   $scope.goToRule = function() {
