ba00f095ff0ff3c8ee1485d1afdae7d288efe358
wangkaijie <wangkaijie@hoolai.com>
2015-01-09 16:51:36 +0800

    JShint

--- app/scripts/service/register/register-service.js
+++ app/scripts/service/register/register-service.js
@@ -1,19 +1,31 @@
 'use strict';
-hongcaiApp.factory('RegisterService', function ($resource, DEFAULT_DOMAIN) {
-	return {
-		saveRegister: $resource(DEFAULT_DOMAIN + '/siteUser/register', {}, {
-			save: {method: "POST", params: {
-				name: '@name',
-				type: '@type',
-				account: '@account',
-				password: '@password',
-				captcha: '@captcha',
-				inviteCode: '@inviteCode',
-        from: '@from'}}
-		}),
-    sendMobileCaptcha: $resource(DEFAULT_DOMAIN + '/siteUser/sendMobileCaptcha', {}, {
-        save: {method: "POST", params: {mobile: '@mobile'}}
-    }),
-    activeEmail: $resource(DEFAULT_DOMAIN + '/siteUser/activeEmail', {etoken: '@etoken'},{})
-	};
-});
+angular.module('hongcaiApp')
+  .factory('RegisterService', function($resource, DEFAULT_DOMAIN) {
+    return {
+      saveRegister: $resource(DEFAULT_DOMAIN + '/siteUser/register', {}, {
+        save: {
+          method: 'POST',
+          params: {
+            name: '@name',
+            type: '@type',
+            account: '@account',
+            password: '@password',
+            captcha: '@captcha',
+            inviteCode: '@inviteCode',
+            from: '@from'
+          }
+        }
+      }),
+      sendMobileCaptcha: $resource(DEFAULT_DOMAIN + '/siteUser/sendMobileCaptcha', {}, {
+        save: {
+          method: 'POST',
+          params: {
+            mobile: '@mobile'
+          }
+        }
+      }),
+      activeEmail: $resource(DEFAULT_DOMAIN + '/siteUser/activeEmail', {
+        etoken: '@etoken'
+      }, {})
+    };
+  });
