fd0d1ace5139862ebe5aa0d83905327dc8caf93b
wangkaijie <wangkaijie@hoolai.com>
2015-01-09 16:40:52 +0800

    about-us 页面的整体变更

--- app/scripts/controller/about-us/about-us-ctrl.js
+++ app/scripts/controller/about-us/about-us-ctrl.js
@@ -1,32 +1,33 @@
 'use strict';
-hongcaiApp.controller('AboutUsCtrl', ['$scope', '$state', '$rootScope', '$location', '$stateParams', 'AboutUsService', function ($scope, $state, $rootScope, $location, $stateParams, AboutUsService) {
-	$scope.menus = {
-            'left': [{
-            	'href': '/introduction-of-platform',
-                'link': 'root.about-us.introduction-of-platform',
-                'text': '宏财简介'},
-            {
-                'href': '/hongcai-trends',
-                'link': 'root.about-us.hongcai-trends',
-                'text': '宏财动态'},
-            {
-                'href': '/web-site-notice',
-                'link': 'root.about-us.web-site-notice',
-                'text': '网站公告'},
-            {
-            	'href': '/company-profile',
-                'link': 'root.about-us.company-profile',
-                'text': '加入宏财'},
-            {
-            	'href': '/media-reports',
-                'link': 'root.about-us.media-reports',
-                'text': '媒体报道'},
-            {
-            	'href': '/link-us',
-                'link': 'root.about-us.link-us',
-                'text': '联系我们'}]
-        };
+angular.module('hongcaiApp')
+  .controller('AboutUsCtrl', ['$scope', '$state', '$rootScope', '$location', function($scope, $state, $rootScope, $location) {
+    $scope.menus = {
+      'left': [{
+        'href': '/introduction-of-platform',
+        'link': 'root.about-us.introduction-of-platform',
+        'text': '宏财简介'
+      }, {
+        'href': '/hongcai-trends',
+        'link': 'root.about-us.hongcai-trends',
+        'text': '宏财动态'
+      }, {
+        'href': '/web-site-notice',
+        'link': 'root.about-us.web-site-notice',
+        'text': '网站公告'
+      }, {
+        'href': '/company-profile',
+        'link': 'root.about-us.company-profile',
+        'text': '加入宏财'
+      }, {
+        'href': '/media-reports',
+        'link': 'root.about-us.media-reports',
+        'text': '媒体报道'
+      }, {
+        'href': '/link-us',
+        'link': 'root.about-us.link-us',
+        'text': '联系我们'
+      }]
+    };
 
     $rootScope.selectPage = $location.path().split('/')[1];
-
-}]);
+  }]);
--- app/scripts/controller/about-us/hongcai-trends-detail-ctrl.js
+++ app/scripts/controller/about-us/hongcai-trends-detail-ctrl.js
@@ -1,8 +1,10 @@
 'use strict';
-hongcaiApp.controller('HongcaiTrendsDetailCtrl', ['$scope', '$state', '$stateParams', 'AboutUsService', function ($scope, $state, $stateParams, AboutUsService) {
-    AboutUsService.textDetail.get({textId: $stateParams.textId}, function(response) {
-        $scope.text = response.data.text;
-        $scope.baseFileUrl = response.data.baseFileUrl;
-        console.log(response.data)
+angular.module('hongcaiApp')
+  .controller('HongcaiTrendsDetailCtrl', ['$scope', '$state', '$stateParams', 'AboutUsService', function($scope, $state, $stateParams, AboutUsService) {
+    AboutUsService.textDetail.get({
+      textId: $stateParams.textId
+    }, function(response) {
+      $scope.text = response.data.text;
+      $scope.baseFileUrl = response.data.baseFileUrl;
     });
-}]);
+  }]);
