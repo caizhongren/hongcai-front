8970e0e5fd469f69006f8b9b93f17a3666d1a71f
wangkaijie <wangkaijie@hoolai.com>
2015-01-16 16:37:42 +0800

    预约详情页的图标显示

--- app/scripts/controller/project/project-details-ctrl.js
+++ app/scripts/controller/project/project-details-ctrl.js
@@ -256,6 +256,35 @@ angular.module('hongcaiApp')
       });
     }
 
+    // 某宝宝的收益率
+    // 接口在这里。参照account-overview-ctrl.js line: 9-55
+    $scope.lineProjectData = {
+      labels: ['January', 'February', 'March'],
+      datasets: [{
+        label: 'My First dataset',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [65, 59, 80]
+      }, {
+        label: 'My Second dataset',
+        fillColor: 'rgba(151,187,205,0.2)',
+        strokeColor: 'rgba(151,187,205,1)',
+        pointColor: 'rgba(151,187,205,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(151,187,205,1)',
+        data: [28, 48, 40]
+      }]
+    }
+
+    $scope.lineProjectOptions = {
+
+    }
+
 
   }]);
 
