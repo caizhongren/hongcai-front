79b710794facf67cdf0cb45c88f6316bac1fb5ef
crystal <gisele6@126.com>
2015-02-02 18:21:49 +0800

    展开/收起-fixed

--- app/scripts/controller/project/project-sponsorInstitution-ctrl.js
+++ app/scripts/controller/project/project-sponsorInstitution-ctrl.js
@@ -63,16 +63,16 @@ angular.module('hongcaiApp')
       $scope.changeScale = function(){
           if($('.sponsor-description .fa').hasClass('fa-arrow-down')){
               $('.sponsor-description').css({'height':'auto'});
-              $('.fa').removeClass('fa-arrow-down');
-              $('.fa').addClass('fa-arrow-up');
-              $('.fa').css({'right':'20px'});
-              $('.fa').text('收起');
+              $('.sponsor-description .fa').removeClass('fa-arrow-down');
+              $('.sponsor-description .fa').addClass('fa-arrow-up');
+              $('.sponsor-description .fa').css({'right':'20px'});
+              $('.sponsor-description .fa').text('收起');
           }else {
               $('.sponsor-description').css({'height':'363px'});
-              $('.fa').removeClass('fa-arrow-up');
-              $('.fa').addClass('fa-arrow-down');
-              $('.fa').css({'right':'33.4%'});
-              $('.fa').text('展开');
+              $('.sponsor-description .fa').removeClass('fa-arrow-up');
+              $('.sponsor-description .fa').addClass('fa-arrow-down');
+              $('.sponsor-description .fa').css({'right':'33.4%'});
+              $('.sponsor-description .fa').text('展开');
           }
 
       };
