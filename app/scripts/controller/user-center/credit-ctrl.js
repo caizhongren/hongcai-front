ce0f5efec22a16704dbd85b0f4c7bf95eada0459
wangkaijie <wangkaijie@hoolai.com>
2015-02-10 11:25:53 +0800

    解决a 标签 ng-click 和 ng-disabled 生效的问题。

--- app/scripts/controller/user-center/credit-ctrl.js
+++ app/scripts/controller/user-center/credit-ctrl.js
@@ -3,7 +3,19 @@ angular.module('hongcaiApp')
   .controller('CreditCtrl', ['$location', '$scope', '$http', '$rootScope', '$state', '$stateParams', 'UserCenterService', '$aside', '$window', 'OrderService', 'config', 'toaster', function($location, $scope, $http, $rootScope, $state, $stateParams, UserCenterService, $aside, $window, OrderService, config, toaster) {
     $rootScope.redirectUrl = $location.path();
     $rootScope.selectSide = 'credit';
-
+    // 第一步
     $scope.creditStepFlag = 1;
     $scope.Math = window.Math;
+    $scope.disabledFlag1 = $scope.disabledFlag2 = $scope.disabledFlag3 = false;
+    // 解决ng-click ng-disabled的生效的问题。
+    $scope.$watch('creditStepFlag', function() {
+      if ($scope.creditStepFlag === 1) {
+        $scope.disabledFlag2 = true;
+        $scope.disabledFlag3 = true;
+      } else if ($scope.creditStepFlag === 2){
+        $scope.disabledFlag3 = true;
+      } else {
+        $scope.disabledFlag2 = false;
+      }
+    });
   }]);
