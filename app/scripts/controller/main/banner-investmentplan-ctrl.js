ea6127d407718ecea9afc21b4d6303eb0b8a5966
dongyao <dongyao@hoolai.com>
2015-03-16 17:50:02 +0800

    网站优化

--- app/scripts/controller/main/banner-investmentplan-ctrl.js
+++ app/scripts/controller/main/banner-investmentplan-ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('hongcaiApp')
-  .controller('BannerInvPlanCtrl', ['$scope', 'MainService', function($scope, MainService) {
+  .controller('BannerInvPlanCtrl', ['$scope', 'MainService', '$location', function($scope, MainService, $location) {
     //  宏金盈列表
     MainService.getIndexFundsProductList.get(function(response) {
       if (response.ret === 1) {
@@ -8,4 +8,10 @@ angular.module('hongcaiApp')
         $scope.fundsProjectProductList = response.data.fundsProjectProductList;
       }
     });
+    $scope.hsh = $location.hash();
+
+    $scope.addActive = function($event) {
+      angular.element($event.target).closest('.col-xs-8').find('.active').removeClass('active');
+      angular.element($event.target).closest('li').find('a').addClass('active');
+    };
   }]);
