e6d8ab8293d4a76eaef7672d80c9c0f62a7709c6
crystal <gisele6@126.com>
2015-03-19 16:36:41 +0800

    投资回调页

--- app/scripts/controller/order/transfer-success-ctrl.js
+++ app/scripts/controller/order/transfer-success-ctrl.js
@@ -1,6 +1,19 @@
 'use strict';
 angular.module('hongcaiApp')
-  .controller('TransferSuccessCtrl', ['$scope', function ($scope) {
-      $scope.page = 2;
+  .controller('TransferSuccessCtrl', ['$scope', '$stateParams', 'ProjectService', function ($scope, $stateParams, ProjectService) {
+    $scope.page = 2;
+    console.log(3);
+    if($stateParams.number){
+      ProjectService.getOneDayProfitAndNextRate.get({
+        number: $stateParams.number
+      }, function(response) {
+        if (response.ret === 1) {
+          
+        } else {
+          console.log(response);
+        }
+      });
+    }
+      
   }]);
 
