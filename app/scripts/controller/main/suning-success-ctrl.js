2cdf5a31d084387f6055935e5f278030247cadc7
wangyadan <wangyadan@hoolai.com>
2015-08-07 17:56:31 +0800

    苏宁活动优化

--- app/scripts/controller/main/suning-success-ctrl.js
+++ app/scripts/controller/main/suning-success-ctrl.js
@@ -3,8 +3,8 @@ angular.module('hongcaiApp')
   .controller('SuningSuccessCtrl', ['$scope', '$rootScope','$stateParams',function ($scope,$rootScope,$stateParams) {
      console.log($stateParams);
      if($stateParams.SuccessStatus == 1){
-     	$scope.sMessage = "恭喜您，成功登录！";
+     	$scope.sMessage = "您已成功登录宏财！";
      }else if($stateParams.SuccessStatus == 2){
-     	$scope.sMessage = "恭喜您，成功注册！";
+     	$scope.sMessage = "恭喜您成功注册宏财！";
      }
   }]);
