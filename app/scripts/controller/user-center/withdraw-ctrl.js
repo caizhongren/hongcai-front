392884b446546c53453454919a01cabbbc945dde
zhangfeng <zhangfeng@hoolai.com>
2015-10-29 15:59:16 +0800

    绑定体现页数据

--- app/scripts/controller/user-center/withdraw-ctrl.js
+++ app/scripts/controller/user-center/withdraw-ctrl.js
@@ -1,6 +1,9 @@
 'use strict';
 angular.module('hongcaiApp')
-  .controller('WithdrawCtrl', ['$location', '$scope', '$state', '$rootScope', '$stateParams', 'UserCenterService', 'DEFAULT_DOMAIN', '$alert', function($location, $scope, $state, $rootScope, $stateParams, UserCenterService, DEFAULT_DOMAIN, $alert) {
+  .controller('WithdrawCtrl', function($location, $scope, $state, $rootScope, $stateParams, UserCenterService, DEFAULT_DOMAIN, $alert) {
+    var currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    $scope.nextDay = currentDate;
 
     $rootScope.selectSide = $location.path().substr($location.path().indexOf('/') + 1);
     $scope.availableCash = 0;
@@ -14,12 +17,9 @@ angular.module('hongcaiApp')
         console.log('ask withdraw, why getUserAvailableCash did not load data...');
       }
     });
+    
     $scope.checkLargestAmount = function(amount) {
-      if (amount > $scope.availableCashRealNo) {
-        return true;
-      } else {
-        return false;
-      }
+        return amount > $scope.availableCashRealNo;
     };
 
     $scope.getPicCaptcha = DEFAULT_DOMAIN + '/siteUser/getPicCaptcha?' + Math.random();
@@ -27,9 +27,6 @@ angular.module('hongcaiApp')
       angular.element('#checkCaptcha').attr('src', angular.element('#checkCaptcha').attr('src').substr(0, angular.element('#checkCaptcha').attr('src').indexOf('?')) + '?code=' + Math.random());
     };
 
-    $scope.reload = function() {
-      window.location.reload();
-    };
 
     $scope.withdraw = function(amount, captcha) {
       $scope.msg = '3';
@@ -42,24 +39,6 @@ angular.module('hongcaiApp')
 
       window.open('/#!/withdraw-transfer/' + amount + '/' + captcha);
 
-      /*UserCenterService.yeepayWithdraw.get({
-        amount: amount,
-        captcha: captcha
-      }, function(response) {
-        if (response.ret === 1) {
-          var req = response.data.req;
-          var sign = response.data.sign;
-          var _f = newForm();
-          createElements(_f, 'req', req);
-          createElements(_f, 'sign', sign);
-          _f.action = config.YEEPAY_ADDRESS + 'toWithdraw';
-          _f.submit();
-        } else if (response.ret === -1) {
-          $scope.msg = response.msg;
-          $alert({scope: $scope, template: 'views/modal/alert-dialog.html', show: true});
-        } else {
-          console.log('ask withdraw, why yeepayWithdraw did not load data...');
-        }
-      });*/
     };
-  }]);
+
+  });
