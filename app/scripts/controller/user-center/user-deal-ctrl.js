b79be7466a78142a7c6304374f992bb3e6b15a60
zhangfeng <zhangfeng@hoolai.com>
2015-10-29 15:31:52 +0800

    优化资金流水页js

--- app/scripts/controller/user-center/user-deal-ctrl.js
+++ app/scripts/controller/user-center/user-deal-ctrl.js
@@ -1,15 +1,17 @@
 'use strict';
 angular.module('hongcaiApp')
-  .controller('UserDealCtrl', ['$scope', '$rootScope', '$state', 'toaster','$stateParams','$location', 'UserCenterService', function ($scope,$rootScope, $state, toaster, $stateParams, $location, UserCenterService) {
+  .controller('UserDealCtrl', function ($scope, $rootScope, toaster, UserCenterService) {
     $rootScope.selectSide = 'record';
-    $scope.type = $stateParams.type || 0;
-    $scope.dateInterval = $stateParams.dateInterval || 0;
+    $scope.type = 0;
+    $scope.dateInterval = 0;
     $scope.dealType = 0;
+
     $scope.recordSelect = function() {
-      // var getDealByUser = UserCenterService.getDealByUser.get({ dateInterval: $stateParams.dateInterval,type: $stateParams.type},function() {
-      var getDealByUser = UserCenterService.getDealByUser.get({ dateInterval: $stateParams.dateInterval,dealType: $scope.dealType},function(response) {
+      var getDealByUser = UserCenterService.getDealByUser.get({ 
+        dateInterval: $scope.dateInterval,
+        dealType: $scope.dealType
+      },function(response) {
         if (getDealByUser.ret === 1) {
-          // console.log(getDealByUser);
           $scope.dealList = getDealByUser.data.dealList;
           $scope.type = getDealByUser.data.type;
           $scope.dateInterval = getDealByUser.data.dateInterval;
@@ -23,6 +25,7 @@ angular.module('hongcaiApp')
           $scope.numberOfPages = function() {
             return Math.ceil($scope.data.length / $scope.pageSize);
           };
+
           for (var i = 0; i < $scope.dealList.length; i++) {
             $scope.data.push($scope.dealList[i]);
           }
@@ -48,5 +51,5 @@ angular.module('hongcaiApp')
     /*$scope.selectedIcon = 'Heart';
     $scope.selectedIcons = '["Gear"]';
     $scope.icons = '[{"value":"Gear","label":"<i class=\"fa fa-gear\"></i> Gear"},{"value":"Globe","label":"<i class=\"fa fa-globe\"></i> Globe"},{"value":"Heart","label":"<i class=\"fa fa-heart\"></i> Heart"},{"value":"Camera","label":"<i class=\"fa fa-camera\"></i> Camera"}]';*/
-  }]);
+  });
 
