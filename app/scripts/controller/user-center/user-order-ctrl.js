974042e0ce003129ff09e95125b942f98d720bcc
zhangfeng <zhangfeng@hoolai.com>
2015-10-30 14:03:59 +0800

    移除无用的代码

--- app/scripts/controller/user-center/user-order-ctrl.js
+++ app/scripts/controller/user-center/user-order-ctrl.js
@@ -22,12 +22,7 @@ angular.module('hongcaiApp')
       }, 300);
     };
 
-    $scope.showListDetails = function(number) {
-      angular.element('#investment-detail').animate({
-        width: 'show'
-      }, 300);
-      $scope.getOrderBillByOrderId(number);
-    };
+  
 
     $scope.generateContractPDF = function(projectId, orderId, status, type) {
       if (status === 2) {
@@ -62,15 +57,7 @@ angular.module('hongcaiApp')
 
     };
 
-    //判断是否开通第三方托管账户
-    $scope.checkTrusteeshipAccount = function() {
-      if ( $rootScope.securityStatus.trusteeshipAccountStatus === 1) {
-        $scope.haveTrusteeshipAccount = true;
-      } else {
-        $scope.haveTrusteeshipAccount = false;
-      }
-      return $scope.haveTrusteeshipAccount;
-    };
+  
 
 
     $scope.loadOrders = function(){
@@ -82,7 +69,6 @@ angular.module('hongcaiApp')
       function(response) {
         if (getOrderByUser.ret === 1) {
           // console.log(getOrderByUser);
-          // $scope.haveTrusteeshipAccount = $scope.checkTrusteeshipAccount();
           // if($scope.haveTrusteeshipAccount) {
           $scope.orderList = getOrderByUser.data.orderProjectList;
           $scope.orderCount = getOrderByUser.data.orderCount;
@@ -149,146 +135,6 @@ angular.module('hongcaiApp')
       }
     };
 
-
-    // 获取详情按钮
-    $scope.getOrderBillByOrderId = function(number) {
-      UserCenterService.getOrderBillByOrderId.get({
-        number: number
-      }, function(response) {
-        if (response.ret === 1) {
-          var invTotal = response.data.order.orderAmount;
-          if (response.data.project) {
-            var rdp = response.data.project;
-            //总融资额
-            var invInitDate = moment(rdp.valueDate).toString();
-            var accountDay = rdp.accountDay;
-            var invStartDate = moment([moment(invInitDate).year(), moment(invInitDate).month(), accountDay]).toString();
-            invStartDate = moment(invStartDate).add(1, 'month').toString();
-            var invEndDate = moment(rdp.repaymentDate).toString();
-            var invCycle = rdp.cycle;
-            var invType = rdp.type;
-            var invRate = rdp.annualEarnings / 100;
-            if (invType === 1) {
-              // 先息后本
-              everyMonthInterestPri(invTotal, invInitDate, invStartDate, invEndDate, invCycle, invRate);
-            } else if (invType === 2) {
-              everyMonthInterestEq(invTotal, invInitDate, invStartDate, invEndDate, invCycle, invRate);
-              // 等额本息
-            }
-            if (response.data.billList) {
-              var bill = response.data.billList;
-              var billList = {};
-              for (var i = 0; i < bill.length; i++) {
-                billList = {
-                  'payDate': moment(bill[i].successTime).format('YYYY-MM-DD'),
-                  'invEarnings': bill[i].amount,
-                  'invStatus': bill[i].status
-                };
-                $scope.listInvPond.splice(i, 1, billList);
-              }
-            }
-            $scope.paid = 0;
-            $scope.unpaid = 0;
-            for (var x = 0; x < $scope.listInvPond.length; x++) {
-              var status = $scope.listInvPond[x].invStatus;
-              if (status === 1) {
-                $scope.paid += $scope.listInvPond[x].invEarnings;
-              } else {
-                $scope.unpaid = $scope.unpaid + $scope.listInvPond[x].invEarnings;
-              }
-            }
-          }
-        }
-      });
-    };
-    var everyMonthInterestPri = function(invTotal, invInitDate, invStartDate, invEndDate, invCycle, invRate) {
-      // 每月的付费天数，付费日期，上次支付日期，该月的利息；
-      $scope.listInvPond = [];
-      var invDays, payDate, prevDate, invEarnings;
-      var invList = {};
-      if (invCycle === 1) {
-        invDays = moment(invEndDate).diff(moment(invInitDate), 'days', true);
-        invEarnings = invTotal + invTotal * invRate * Math.round(invDays) / 365; //计算利率
-        invList = {
-          'payDate': moment(invEndDate).format('YYYY-MM-DD'),
-          'invEarnings': invEarnings,
-          'invStatus': '0'
-        };
-        $scope.listInvPond.push(invList);
-      } else {
-        // var LastPayDate = moment(invStartDate).add((invCycle - 1), 'month').toString();
-        // LastPayDate 永远等于 invEndDate?
-        // var diffDate = moment(LastPayDate).diff(moment(invEndDate), 'days');
-        // if (diffDate === 0) {
-        //   invCycle = invCycle - 1;
-        // }
-        // 原先是这样
-        invCycle = invCycle - 1;
-        for (var i = 0; i <= invCycle; i++) {
-          payDate = moment(invStartDate).add(i, 'month').toString();
-          if (moment(payDate) > moment(invEndDate)) {
-            payDate = invEndDate;
-          }
-          if (i === 0) {
-            invDays = moment(payDate).diff(moment(invInitDate), 'days', true) + 1;
-          } else {
-            invDays = moment(payDate).diff(moment(prevDate), 'days', true);
-          }
-          invEarnings = invTotal * invRate * Math.round(invDays) / 365; //计算利率
-          if (i === invCycle) {
-            invEarnings = invEarnings + invTotal;
-          }
-          prevDate = payDate;
-          invList = {
-            'payDate': moment(payDate).format('YYYY-MM-DD'),
-            'invEarnings': invEarnings,
-            'invStatus': '0'
-          };
-          $scope.listInvPond.push(invList);
-        }
-      }
-    };
-    // 等额本息  intType = 2
-    var everyMonthInterestEq = function(invTotal, invInitDate, invStartDate, invEndDate, invCycle, invRate) {
-      $scope.listInvPond = [];
-      var invDays, payDate, prevDate, invEarnings, currentMonthInterest, payDiffDate; //每月的付费天数，付费日期，上次支付日期，该月的付款金额, 当月生成的的利息；
-      var invList = {};
-      invEarnings = (invTotal * invRate * Math.pow((1 + invRate / invCycle), invCycle)) / (Math.pow((1 + invRate / invCycle), invCycle) - 1) / invCycle;
-      var LastPayDate = moment(invStartDate).add(invCycle, 'month').toString();
-      var diffDate = moment(LastPayDate).diff(moment(invEndDate), 'days');
-      if (diffDate === 0) {
-        invCycle = invCycle - 1;
-      }
-      invCycle = invCycle - 1;
-      for (var i = 0; i <= invCycle; i++) {
-        payDate = moment(invStartDate).add(i, 'month').toString();
-        if (diffDate !== 0) {
-          payDiffDate = moment(payDate).diff(moment(invEndDate), 'days');
-          if (payDiffDate > 0) {
-            payDate = invEndDate;
-            invDays = moment(payDate).diff(moment(prevDate), 'days', true);
-            invEarnings = invTotal + invTotal * invRate * Math.round(invDays) / 365;
-            invList = {
-              'payDate': moment(payDate).format('YYYY-MM-DD'),
-              'invEarnings': invEarnings,
-              'invStatus': '0'
-            };
-            $scope.listInvPond.push(invList);
-            break;
-          } else {
-            currentMonthInterest = invTotal * invRate / 12;
-            invTotal = invTotal - (invEarnings - currentMonthInterest);
-          }
-        }
-        prevDate = payDate;
-        invList = {
-          'payDate': moment(payDate).format('YYYY-MM-DD'),
-          'invEarnings': invEarnings,
-          'invStatus': '0'
-        };
-        $scope.listInvPond.push(invList);
-      }
-    };
 
     
     // Based on an implementation here: web.student.tuwien.ac.at/~e0427417/jsdownload.html
