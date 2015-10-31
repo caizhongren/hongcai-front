4fddb9fd15670480e36a5eff7466c69ea70243ae
zhangfeng <zhangfeng@hoolai.com>
2015-10-31 14:58:35 +0800

    资产总览也波形图

--- app/scripts/controller/user-center/assets-overview-ctrl.js
+++ app/scripts/controller/user-center/assets-overview-ctrl.js
@@ -3,10 +3,13 @@ angular.module('hongcaiApp')
   .controller('AssetsOverviewCtrl', ['$scope', '$state', '$rootScope', '$stateParams', 'UserCenterService', function($scope, $state, $rootScope, $stateParams, UserCenterService) {
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A'];
-    $scope.data = [
-      [50, 250, 150, 200, 350, 50, 400]
-    ];
+     $scope.data = [
+       [28, 48, 40, 19, 86, 27, 90]
+     ];
+     $scope.onClick = function (points, evt) {
+       console.log(points, evt);
+     };
+     
     $scope.options = {
       bezierCurve: false,
       scaleShowVerticalLines: false
@@ -34,67 +37,6 @@ angular.module('hongcaiApp')
 
         $scope.reward = reward;
         $scope.account = response.data.account;
-        if (balance === 0 && waitingProfit === 0 && waitingCapital === 0 && freezeCapital === 0 && receivedProfit === 0 && amount === 0) {
-          $scope.doughnutAssetsData = [{
-            value: 20,
-            label: '可用余额',
-            color: '#d2cb3f'
-          }, {
-            value: 20,
-            label: '待收收益',
-            color: '#62cbc6'
-          }, {
-            value: 20,
-            label: '待收本金',
-            color: '#f9b81e'
-          }, {
-            value: 20,
-            label: '冻结资金',
-            color: '#6aabe1'
-          }];
-        } else {
-          if (reward > 0) {
-            $scope.doughnutAssetsData = [{
-              value: account.balance,
-              label: '可用余额',
-              color: '#d2cb3f'
-            }, {
-              value: account.waitingProfit,
-              label: '待收收益',
-              color: '#62cbc6'
-            }, {
-              value: account.waitingCapital,
-              label: '待收本金',
-              color: '#f9b81e'
-            }, {
-              value: account.freezeCapital,
-              label: '冻结资金',
-              color: '#6aabe1'
-            }];
-          } else if (reward === 0) {
-            $scope.doughnutAssetsData = [{
-              value: account.balance,
-              label: '可用余额',
-              color: '#d2cb3f'
-            }, {
-              value: account.waitingProfit,
-              label: '待收收益',
-              color: '#62cbc6'
-            }, {
-              value: account.waitingCapital,
-              label: '待收本金',
-              color: '#f9b81e'
-            }, {
-              value: account.freezeCapital,
-              label: '冻结资金',
-              color: '#6aabe1'
-            }, {
-              value: reward,
-              label: '活动奖金',
-              color: '#cb62bb'
-            }];
-          }
-        }
 
       } else {
         console.log('ask assets-overview, why getUserAccount did not load data...');
@@ -102,30 +44,4 @@ angular.module('hongcaiApp')
       }
     });
 
-    if (balance > 0 && waitingProfit > 0 && waitingCapital > 0 && freezeCapital > 0 && receivedProfit > 0 && amount > 0) {
-      $scope.doughnutOptions = {
-        segmentShowStroke: false,
-        segmentStrokeColor: '#fff',
-        segmentStrokeWidth: 2,
-        percentageInnerCutout: 65,
-        animation: true,
-        animationSteps: 100,
-        animationEasing: 'easeOutQuart',
-        animateRotate: true,
-        animateScale: false
-      };
-    } else {
-      $scope.doughnutOptions = {
-        segmentShowStroke: false,
-        segmentStrokeColor: '#fff',
-        segmentStrokeWidth: 2,
-        percentageInnerCutout: 65,
-        animation: true,
-        animationSteps: 100,
-        animationEasing: 'easeOutQuart',
-        animateRotate: true,
-        animateScale: false,
-        showTooltips: false
-      };
-    }
   }]);
