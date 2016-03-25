5fefe1e07354ac842d364cb12b13f3b3300c49dd
zhangfeng <zhangfeng@hoolai.com>
2016-03-25 17:48:23 +0800

    refactor(messageCtrl)：增加分页机制

--- app/scripts/controller/user-center/message-ctrl.js
+++ app/scripts/controller/user-center/message-ctrl.js
@@ -1,72 +1,90 @@
 'use strict';
 angular.module('hongcaiApp')
-  .controller('MessageCtrl', ['$location', '$scope', 'toaster', '$state', '$rootScope', '$stateParams', 'UserCenterService', function($location, $scope, toaster, $state, $rootScope, $stateParams, UserCenterService) {
+  .controller('MessageCtrl', function($location, $scope, toaster, $state, $rootScope, $stateParams, UserCenterService) {
+    $rootScope.pageTitle = '站内信-要理财，上宏财！';
+
     $rootScope.selectSide = 'message';
-    UserCenterService.getUserMsgByStatus.get({
-      status: $stateParams.status
-    }, function(response) {
-      $scope.status = $stateParams.status;
-      if (response.ret === 1) {
-        $scope.userMsgList = response.data.userMsgList;
-        $scope.orderProp = 'id';
-        $scope.currentPage = 0;
-        $scope.pageSize = 10;
-        $scope.data = [];
-        $scope.numberOfPages = function() {
-          return Math.ceil($scope.data.length / $scope.pageSize);
-        };
-        for (var i = 0; i < $scope.userMsgList.length; i++) {
-          $scope.data.push($scope.userMsgList[i]);
-        }
 
-        // $scope.changeStatus = function(status,id,$event,$index){
-        $scope.changeStatus = function(status, id, $index) {
-          var index = $index + 1;
-          var targetP = $('.list-group-item').eq(index).find('p');
 
-          if (targetP.hasClass('unfold')) {
-            targetP.removeClass('unfold');
-          } else {
-            targetP.addClass('unfold');
-          }
+    // $scope.changeStatus = function(status,id,$event,$index){
+    $scope.changeStatus = function(status, id, $index) {
+      var index = $index + 1;
+      var targetP = $('.list-group-item').eq(index).find('p');
 
-          if (status === 0) {
-            //markSingleMsgRead
-            UserCenterService.readOneMsg.get({
-              'userMsgId': id
-            }, function(response) {
+      if (targetP.hasClass('unfold')) {
+        targetP.removeClass('unfold');
+      } else {
+        targetP.addClass('unfold');
+      }
+
+      if (status === 0) {
+        //markSingleMsgRead
+        UserCenterService.readOneMsg.get({
+          'userMsgId': id
+        }, function(response) {
+          if (response.ret === 1) {
+            UserCenterService.getUnreadMsgCount.get(function(response) {
               if (response.ret === 1) {
-                UserCenterService.getUnreadMsgCount.get(function(response) {
-                  if (response.ret === 1) {
-                    $rootScope.unreadCount = response.data.unreadCount;
-                  }
-                });
-              } else {
-                toaster.pop('error', response.msg);
+                $rootScope.unreadCount = response.data.unreadCount;
               }
             });
-
-            $('.list-group-item').eq(index).removeClass('unread-flag');
-
+          } else {
+            toaster.pop('error', response.msg);
           }
-        };
+        });
 
-        //markAllMsgRead
-        $scope.updateAllMsgStatus = function() {
-          UserCenterService.readAllMsg.get({}, function(response) {
+        $('.list-group-item').eq(index).removeClass('unread-flag');
+
+      }
+    };
+
+    $scope.currentPage = 1;
+    $scope.pageSize = 10;
+    $scope.status = $stateParams.status;
+
+    
+
+    /**
+     * 加载更多
+     */
+    $scope.loadPage = function(page, pageSize, status){
+
+      UserCenterService.getUserMsgByStatus.get({
+        status: status
+      }, function(response) {
+        if (response.ret === 1) {
+          $scope.currentPage = page;
+          $scope.pageSize = pageSize;
+          $scope.status = status;
+
+          $scope.count = response.data.count;
+          $scope.orderProp = 'id';
+          $scope.data = response.data.userMsgList;
+          $scope.numberOfPages = function() {
+            return Math.ceil($scope.count / $scope.pageSize);
+          };
+        }
+      });
+
+    }
+
+    $scope.loadPage($scope.currentPage, $scope.pageSize, $stateParams.status);
+
+
+    //markAllMsgRead
+    $scope.updateAllMsgStatus = function() {
+      UserCenterService.readAllMsg.get({}, function(response) {
+        if (response.ret === 1) {
+          angular.element('li[class]').removeClass('unread-flag');
+          UserCenterService.getUnreadMsgCount.get(function(response) {
             if (response.ret === 1) {
-              angular.element('li[class]').removeClass('unread-flag');
-              UserCenterService.getUnreadMsgCount.get(function(response) {
-                if (response.ret === 1) {
-                  $rootScope.unreadCount = response.data.unreadCount;
-                }
-              });
-            } else {
-              toaster.pop('error', response.msg);
+              $rootScope.unreadCount = response.data.unreadCount;
             }
           });
-        };
-      }
-    });
+        } else {
+          toaster.pop('error', response.msg);
+        }
+      });
+    };
 
-  }]);
+  });
