69e77e012e7042e9020403364a980a78b7061bfc
zhangfeng <zhangfeng@hoolai.com>
2016-04-27 15:07:07 +0800

    feat(图片alt属性)：网站图片添加alt属性，包括轮播图

--- app/scripts/controller/main/slider-ctrl.js
+++ app/scripts/controller/main/slider-ctrl.js
@@ -8,27 +8,33 @@ angular.module('hongcaiApp')
     $scope.media = [{
       mimeType: 'image/jpg',
       src: 'images/banner/banner008.jpg',
+      name: '4000元体验金'
     }, {
       mimeType: 'image/jpg',
       src: 'images/banner/banner007.jpg',
-      href: 'https://www.hongcai.com/media-reports-detail/213'
+      href: 'https://www.hongcai.com/media-reports-detail/213',
+      name: '东兴证券、太平洋证券合作'
     }, {
       mimeType: 'image/jpg',
       src: 'images/banner/banner006.jpg',
-      href: 'http://mp.weixin.qq.com/s?__biz=MzA5Nzg0MzA5OQ==&mid=400520084&idx=1&sn=972091ed5d1ebceb6a96a0067cbb1294#rd'
+      href: 'http://mp.weixin.qq.com/s?__biz=MzA5Nzg0MzA5OQ==&mid=400520084&idx=1&sn=972091ed5d1ebceb6a96a0067cbb1294#rd',
+      name: '点赞就送50元'
     }, {
       mimeType: 'image/jpg',
       src: 'images/banner/banner003.jpg',
-      href: '/banner-investmentplan'
+      href: '/banner-investmentplan',
+      name: '宏金盈简介'
 
     }, {
       mimeType: 'image/jpg',
       src: 'images/banner/banner002.jpg',
-      href: '/consultant-team'
+      href: '/consultant-team',
+      name: '专业权威专家团队'
     }, {
       mimeType: 'image/jpg',
       src: 'images/banner/banner005.jpg',
-      href: '/safe'
+      href: '/safe',
+      name: '安全保障'
     }];
 
     // 首页数据统计
