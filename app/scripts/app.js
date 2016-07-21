e5eab3a9b3f908119fcc351f6473eed9b9a3c99d
liujia <liujia@hoolai.com>
2016-07-21 10:13:56 +0800

    feat(app重新获取参数act&f) app重新获取参数act&f

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1974,9 +1974,8 @@ hongcaiApp.run(function($rootScope, $location, $window, $http, $state, $modal, D
     $rootScope.firstPath = $location.path().split('/')[1];
     $rootScope.selectSide = $location.path().split('/')[2];
 
-    $rootScope.act = $state.params.act;
-    $rootScope.channelCode = $state.params.f;
-    console.log(ipCookie());
+    $rootScope.act = $location.search().act;
+    $rootScope.channelCode = $location.search().f;
 
     if ($rootScope.channelCode) {
       ipCookie('utm_from', $rootScope.channelCode, {
