9c208137cc0a7b3f00426279347583b8aaab3653
yuyang <yuyang1@hoolai.com>
2016-08-24 10:35:02 +0800

    feat(modal-activate):调整存管通激活弹窗文案及背景图大小

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1909,7 +1909,6 @@ hongcaiApp.run(function($templateCache, $rootScope, $location, $window, $http, $
       $rootScope.activateModal = $modal({
         scope: $rootScope,
         template: 'views/modal/modal-activate.html',
-        // controller: 'modalActivateCtrl',
         show: true
       });
     }
