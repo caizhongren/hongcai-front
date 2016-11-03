dfed528e80b2bda73dd1adf9533f810e62f2f564
fuqiang <fuqiang@hoolai.com>
2016-11-03 17:32:07 +0800

    feat(credit-detail-ctrl) 添加债券认购接口

--- app/scripts/controller/project/credit-details-ctrl.js
+++ app/scripts/controller/project/credit-details-ctrl.js
@@ -1,10 +1,11 @@
 'use strict';
 angular.module('hongcaiApp')
-  .controller('CreditDetailsCtrl', ['$scope', '$state', '$rootScope', '$location', '$stateParams', 'CreditService', 'OrderService', '$modal', '$alert', 'toaster', '$timeout', 'ipCookie', 'MainService', 'ProjectService', function($scope, $state, $rootScope, $location, $stateParams, CreditService, OrderService, $modal, $alert, toaster, $timeout, ipCookie, MainService, ProjectService) {
+  .controller('CreditDetailsCtrl', function($scope, $state, $rootScope, $location, $stateParams, $window, CreditService, OrderService, $modal, $alert, toaster, $timeout, ipCookie, MainService, ProjectService, RESTFUL_DOMAIN) {
     var number = $stateParams.number;
-    if (!number) {
+    console.log(number);
+    // if (!number) {
       // $state.go('root.credit-list-query-no');
-    }
+    // }
     // CreditService.creditAssignmentDetail.get({
     //   assignmentNumber: number
     // }, function(response) {
@@ -53,10 +54,11 @@ angular.module('hongcaiApp')
      * 债券详情
      */
     CreditService.getCreditAssignment.get({
-      number: $stateParams.number
+      number: number
     },function(response){
-      if(response && response != null) {
+      if(response && response.ret !== -1) {
         $scope.creditProject = response;
+        $scope.creditNum = $scope.creditProject.number;
       }
     })
     /**
@@ -68,7 +70,9 @@ angular.module('hongcaiApp')
        page: page, 
        pageSize: pageSize
       }, function(response) {
-        console.log(response);
+        if(response && response.ret != -1) {
+          console.log(response);
+        }
       });
      };
      $scope.getCreditAssignmentList(1,6);
@@ -80,7 +84,73 @@ angular.module('hongcaiApp')
         number: number,
       })
     }
-    $scope.getProjectBills($stateParams.number);
+    // $scope.getProjectBills($stateParams.number);
+
+    /**
+    * 立即认购
+    */
+    // $scope.amount = 100; 
+    $scope.$watch('test111', function(newVal, oldVal) {
+      console.log(newVal);
+      if( newVal == undefined) {
+        $scope.errMsg = '';
+      } 
+      if(newVal) {
+        if(newVal < 100) {
+          $scope.errMsg = '投资金额必须大于等于100';
+        }
+        // if(newVal % 100 != 0) {
+        //   $scope.errMsg = '投资金额必须为' + $scope.project.increaseAmount + '的整数倍';
+        // }
+        // if(newVal > $scope.creditProject.currentStock && $scope.creditProject.currentStock > $rootScope.account.balance) {
+        //   $scope.errMsg = '账户余额不足，请先充值';
+        // }
+        // if(newVal < $scope.creditProject.currentStock && $scope.creditProject.currentStock < $rootScope.account.balance ) {
+        //   $scope.errMsg = '投资金额必须小于' + $scope.creditProject.currentStock;
+        // }
+      }
+      // $scope.$apply();
+    });
+
+
+
+    
+    $scope.toInvest = function(amount) {
+
+
+      // 使用同步请求， 解决有可能弹窗被浏览器拦截的问题
+      $.ajax({
+        url: RESTFUL_DOMAIN + '/assignments/' + $scope.creditNum + '/orders' + '?amount=' + amount,
+        'type': 'POST',
+        async: false,
+        dataType: 'json',
+        success: function(response) {
+          $scope.msg = '12';
+          $scope.investAmount = amount;
+          if (response && response.ret !== -1) {
+            $alert({
+              scope: $scope,
+              template: 'views/modal/alertYEEPAY.html',
+              show: true
+            });
+
+            $window.open('/#!/user-order-transfer/' + response.projectId + '/' + response.id + '/' + response.type + '?orderNumber=' + response.number, '_blank');
+          } else {
+            toaster.pop('error', response.msg);
+          }
+        }
+      });
+    };
+
+
+
+
+
+
+
+
+
+
 
 
     $scope.toRealNameAuth = function() {
@@ -178,29 +248,13 @@ angular.module('hongcaiApp')
 
     $rootScope.selectPage = $location.path().split('/')[1];
 
-    // $scope.tabs = [{
-    //   title: '还款计划',
-    // }, {
-    //   title: '企业信息',
-    // }, {
-    //   title: '风控信息',
-    // }, {
-    //   title: '相关文件',
-    // }, {
-    //   title: '项目历程',
-    // }];
     $scope.tabs = [{
       title: '投资记录',
     }, {
       title: '还款计划',
     }];
 
-    // $scope.tabsRight = [{
-    //   title: '投资记录',
-    // }, {
-    //   title: '还款计划',
-    // }];
-
+    
     $scope.tabsRightReserve = [{
       title: '当前预约',
     }, {
@@ -226,4 +280,64 @@ angular.module('hongcaiApp')
         redirectUrl: thisUrl
       });
     };
-  }]);
+
+    $scope.datas = [{
+      time: '2016-01-10',
+      anuual: '10',
+      amount: '10000'
+    }, {
+      time: '2016-01-11',
+      anuual: '11',
+      amount: '10000'
+    },  {
+      time: '2016-01-12',
+      anuual: '13',
+      amount: '10000'
+    }, {
+      time: '2016-01-13',
+      anuual: '14',
+      amount: '10000'
+    },  {
+      time: '2016-01-14',
+      anuual: '15',
+      amount: '10000'
+    },  {
+      time: '2016-01-15',
+      anuual: '16',
+      amount: '10000'
+    },  {
+      time: '2016-01-16',
+      anuual: '17',
+      amount: '10000'
+    }, {
+      time: '2016-01-17',
+      anuual: '10',
+      amount: '10000'
+    }, {
+      time: '2016-01-18',
+      anuual: '10',
+      amount: '10000'
+    },  {
+      time: '2016-01-19',
+      anuual: '10',
+      amount: '10000'
+    },  {
+      time: '2016-01-12',
+      anuual: '10',
+      amount: '10000'
+    }, {
+      time: '2016-01-12',
+      anuual: '10',
+      amount: '10000'
+    }];
+    $scope.currentPage0 = 1;
+    $scope.pageSize0 = 7; 
+    $scope.pageCount0 = Math.ceil($scope.datas.length/$scope.pageSize0);
+    $scope.load = function(page) {
+      if(page != $scope.currentPage0) {
+        $scope.currentPage0 = page;
+      }
+
+      $scope.getCreditAssignmentList(page, $scope.pageSize0);
+    }
+  });
--- app/scripts/service/order/order-service.js
+++ app/scripts/service/order/order-service.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('hongcaiApp')
-  .factory('OrderService', function ($resource, DEFAULT_DOMAIN) {
+  .factory('OrderService', function ($resource, DEFAULT_DOMAIN, RESTFUL_DOMAIN) {
     return {
       investVerify: $resource(DEFAULT_DOMAIN + '/siteOrder/investVerify', {projectId: '@projectId',amount: '@amount'}),
       investVerifyFunds: $resource(DEFAULT_DOMAIN + '/siteOrder/investVerifyFunds', {projectId: '@projectId',amount: '@amount', isRepeat: '@isRepeat'}),
@@ -23,6 +23,23 @@ angular.module('hongcaiApp')
         projectId: '@projectId', amount: '@amount', isRepeat: '@isRepeat', payAmount: '@payAmount'
       }),
       getUnUsedIncreaseRateCoupons: $resource(DEFAULT_DOMAIN + '/siteOrder/getUnUsedIncreaseRateCoupons',{projectId : '@projectId', amount : '@amount'}),
-
+      //认购下单
+      investAssignment: $resource(RESTFUL_DOMAIN + '/assignments/:number/orders', {
+        number: '@number',
+        amount: '@amount',
+        device: '@device'
+      },{
+        'POST': {
+          method:'POST'
+        }
+      }),
+      //下单成功
+      transferAssignment: $resource(RESTFUL_DOMAIN + '/orders/:number/users/0/payment', {
+        number: '@number'
+      }, {
+        'POST': {
+          method: 'POST'
+        }
+      })
     };
   });
