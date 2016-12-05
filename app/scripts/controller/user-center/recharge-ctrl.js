295e5d19330c02d64a7858dd54966519f3f60335
fuqiang <fuqiang@hoolai.com>
2016-12-05 19:46:54 +0800

    feat(recharge) 充值页添加联动支付，添加更换银行卡功能

--- app/scripts/controller/user-center/recharge-ctrl.js
+++ app/scripts/controller/user-center/recharge-ctrl.js
@@ -10,12 +10,75 @@ angular.module('hongcaiApp')
       }
     });
 
+    $scope.bankCodeList = {
+      'ICBK':{
+      'imgUrl': '/images/user-center/ICBK.png',
+      'limit': '5w/5w/20w'
+      },
+      'BKCH':{
+      'imgUrl': '/images/user-center/BKCH.png',
+      'limit': '5w/10w/20w'
+      },
+      'PCBC':{
+      'imgUrl': '/images/user-center/PCBC.png',
+      'limit': '5w/10w/20w'
+      },
+      'ABOC':{
+      'imgUrl': '/images/user-center/ABOC.png',
+      'limit': '5w/10w/20w'
+      },
+      'COMM':{
+      'imgUrl': '/images/user-center/COMM.png',
+      'limit': '5w/10w/20w'
+      },
+      'CMBC':{
+      'imgUrl': '/images/user-center/CMBC.png',
+      'limit': '5w/5w/20w'
+      },
+      'CIBK':{
+      'imgUrl': '/images/user-center/CIBK.png',
+      'limit': '5w/20w/20w'
+      },
+      'SZDB':{
+      'imgUrl': '/images/user-center/SZDB.png',
+      'limit': '5w/20w/20w'
+      },
+      'MSBC':{
+      'imgUrl': '/images/user-center/MSBC.png',
+      'limit': '5w/20w/20w'
+      },
+      'EVER':{
+      'imgUrl': '/images/user-center/EVER.png',
+      'limit': '5w/20w/20w'
+      },
+      'HXBK':{
+      'imgUrl': '/images/user-center/HXBK.png',
+      'limit': '5w/20w/20w'
+      },
+      'GDBK':{
+      'imgUrl': '/images/user-center/GDBK.png',
+      'limit': '5w/20w/20w'
+      },
+      'PSBC':{
+      'imgUrl': '/images/user-center/PSBC.png',
+      'limit': '5w/20w/20w'
+      },
+      'FJIB':{
+      'imgUrl': '/images/user-center/FJIB.png',
+      'limit': '5w/5w/20w'
+      },
+      'SPDB':{
+      'imgUrl': '/images/user-center/SPDB.png',
+      'limit': '5w/5w/20w'
+      },
+    }
     /*
      *获取用户已绑定银行卡信息
      */
     UserCenterService.getUserBankCard.get({}, function(response) {
       if (response.ret === 1 && response.data.card) {
         var cardStatus = response.data.card.status;
+        $scope.userCard = response.data.card;
         var userId = response.data.card.userId;
         if (cardStatus === 'VERIFIED') {
           $scope.bankCode = response.data.card.bankCode;
@@ -35,6 +98,20 @@ angular.module('hongcaiApp')
       angular.element('#checkCaptcha').attr('src', angular.element('#checkCaptcha').attr('src').substr(0, angular.element('#checkCaptcha').attr('src').indexOf('?')) + '?code=' + Math.random());
     };
 
+    $scope.unbindBankCard = function() {
+      $acope.showBankCardTip = false;
+      UserCenterService.unbindBankCard.get({}, function(response) {
+        if (response.ret === 1) {
+          $state.go('root.yeepay-callback', {
+            business: 'UNBIND_CARD',
+            status: 'SUCCESS'
+          });
+        } else {
+          toaster.pop('error', response.msg);
+        }
+      });
+    };
+    $scope.showBankCardTip = false;
 
     $scope.transferToPlaform = function(amount) {
       $scope.msg = '8';
