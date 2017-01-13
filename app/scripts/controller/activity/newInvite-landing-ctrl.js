206d7519b33ec7091098d2a24470b54ece537d38
yuyang <yuyang1@hoolai.com>
2017-01-13 18:04:39 +0800

    feat(newInvite-landing-ctrl.js):邀请活动弹窗 宏金保详情页 奖金券 查看更多

--- app/scripts/controller/activity/newInvite-landing-ctrl.js
+++ app/scripts/controller/activity/newInvite-landing-ctrl.js
@@ -7,7 +7,7 @@
 
 'use strict';
 angular.module('hongcaiApp')
-  .controller('newInviteCtrl', function($location, $scope, $rootScope, $state){
+  .controller('newInviteCtrl', function(toaster, $location, $scope, $rootScope, $state, VouchersService){
     $scope.toEarnMoney = function(){
       if($rootScope.isLogged){
         $state.go('root.userCenter.invite-rebate');
@@ -15,4 +15,31 @@ angular.module('hongcaiApp')
         $state.go('root.login', {'redirectUrl': $location.path()});
       }
     }
+
+    // 市场 新年邀请活动
+    $scope.goInvite = function(){
+      if($rootScope.isLogged){
+        $scope.isModal = true;
+        $('#invite').attr({'data-toggle':'modal', 'data-target':'#myModal'});
+      }else {
+        toaster.pop('warning', '对不起，您还未登录，请先登录');
+        // $rootScope.tologin();
+        $state.go('root.login', {'redirectUrl': $location.path()});
+      }
+    }
+    VouchersService.getInviteList.get(function(response) {
+      if (response.ret === 1) {
+        $scope.inviteCode = response.data.voucher.inviteCode;
+
+        // 生成自己的二维码
+		$('#qrcode').qrcode({
+		  render:'table',
+		  text:'http://m.hongcai.com/register?inviteCode=' + $scope.inviteCode,
+		  size: 150
+		});
+      }else {
+
+      }
+    })
+
   })
