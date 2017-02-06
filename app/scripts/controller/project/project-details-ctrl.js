9db6d3240c006ed107ab6f76756f1dfefa6a59e4
fuqiang <fuqiang@hoolai.com>
2017-02-06 13:58:29 +0800

    feat(未支付订单) 未支付订单提示

--- app/scripts/controller/project/project-details-ctrl.js
+++ app/scripts/controller/project/project-details-ctrl.js
@@ -707,7 +707,51 @@ angular.module('hongcaiApp')
     //查看更多
     $scope.initLimit = 8;
     $scope.loadMore = function(){
-      $scope.initLimit = $scope.initLimit + 3 < $scope.list.length ? $scope.initLimit + 3 : $scope.list.length;;
+      $scope.initLimit = $scope.initLimit + 3 < $scope.list.length ? $scope.initLimit + 3 : $scope.list.length;
     }
+    // 未支付订单
+    $scope.showUnfinishedOrder = function() {
+      OrderService.unFinishedOrder.get({}, function(order){
+        $scope.unFinishedOrder = order;
+      })
+    }
+    /**
+     * 继续支付订单
+     */
+    $scope.toPay = function(order) {
+
+      var projectId = order.projectId;
+      var orderId = order.id;
+      var orderType = order.type;
+      var orderAmount = order.orderAmount;
+      var orderNumber = order.number;
+
+      $scope.msg = '12';
+      $scope.investAmount = orderAmount;
+      $scope.page = 'investment';
+      $alert({
+        scope: $scope,
+        template: 'views/modal/alertYEEPAY.html',
+        show: true
+      });
+
+      window.open('/user-order-transfer/' + projectId + '/' + orderId + '/' + orderType + '?orderNumber=' + orderNumber);
+    };
+
+    /**
+     * 取消订单
+     */
+    $scope.cancelOrder = function(number) {
+      UserCenterService.cancelOrder.get({
+        number: number
+      }, function(response) {
+        if (response.ret === 1) {
+          $state.reload();
+          toaster.pop('success', '订单取消成功！');
+        } else {
+          toaster.pop('warning', '无法取消订单，请重试。');
+        }
+      });
+    };
 
   });
