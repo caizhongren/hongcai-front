89026198b2fc5d308bdb10a577dd76f9013d1a55
yuyang <yuyang1@hooali.com>
2017-09-04 18:20:17 +0800

    fix(app_run.js):去掉跳转HTTPS的全局配置

--- app/scripts/app_run.js
+++ app/scripts/app_run.js
@@ -205,12 +205,6 @@ angular.module('hongcaiApp')
       if (config.ignorePATH && config.ignorePATH.indexOf('/' + $location.path().split('/')[1]) !== -1) {
         $location.path('//');
       }
-      /**
-       * 跳转HTTPS的全局配置
-       */
-      if ($location.protocol() === 'http' && config.jumpHttpsPath && config.jumpHttpsPath.indexOf('/' + $location.path().split('/')[1]) !== -1) {
-        $window.location.href = 'https://' + $location.absUrl().split('://')[1];
-      }
 
       $rootScope.firstPath = $location.path().split('/')[1];
       if($location.path().split('/')[1].slice(0,$location.path().split('/')[1].indexOf('?')) == 'assignments'){
