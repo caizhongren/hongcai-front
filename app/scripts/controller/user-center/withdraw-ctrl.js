39d043681b7f2ae97a0389d72a1506ccd5aba1a8
yuyang <yuyang1@hooali.com>
2017-11-23 11:49:19 +0800

    Revert "fix(withdraw):提现页 提现手续费修改"
    This reverts commit 2a9ab4c4b5907de7d3daaf052441d1ccbd78a2a0.

--- app/scripts/controller/user-center/withdraw-ctrl.js
+++ app/scripts/controller/user-center/withdraw-ctrl.js
@@ -17,9 +17,9 @@ angular.module('hongcaiApp')
       if (response && response.ret !== 1) {
         $scope.availableCash = response.account.availableCash;
         var cash = Math.floor($scope.availableCash * 100)/100;
-        $scope.withdrawFee = response.withdrawFee; // 提现手续费
+        var withdrawFee = response.withdrawFee; // 提现手续费
         $scope.cardStatus = response.cardStatus;
-        $scope.availableCashRealNo = cash >= $scope.withdrawFee ? cash - $scope.withdrawFee : 0;
+        $scope.availableCashRealNo = cash >= withdrawFee ? cash - withdrawFee : 0;
       }
     });
     // 本月可免费提现次数查询
