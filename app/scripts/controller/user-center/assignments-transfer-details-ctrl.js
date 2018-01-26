aee0839c04904c64eb4c47f7dcb92f8076210719
yuyang <yuyang1@hooali.com>
2018-01-26 14:17:52 +0800

    feat(about-us):关于我们>>联系我们  公司地址修改

--- app/scripts/controller/user-center/assignments-transfer-details-ctrl.js
+++ app/scripts/controller/user-center/assignments-transfer-details-ctrl.js
@@ -170,14 +170,13 @@ angular.module('hongcaiApp')
           $scope.msg = '转让金额必须小于债权金额';
         }
       }
-      //手续费计算   
-      // 原规则：该笔债权持有时间 < 30天，手续费 = 转让本金 * 1%，持有时间 ≥ 30天，手续费 = 转让本金 * 0.5%，单笔转让的手续费最低为3元。
+      //手续费计算   // 原规则：该笔债权持有时间 < 30天，手续费 = 转让本金 * 1%，持有时间 ≥ 30天，手续费 = 转让本金 * 0.5%，单笔转让的手续费最低为3元。
       // if ($scope.currentDate - $scope.creatTime <= $scope.borderDay*24*60*60*1000) {
       //   $scope.counterFee = $scope.transferAmount * $scope.lessThanOrEqualBorderDayFee / 100 * $scope.discountFeeRate > $scope.minFee ? $scope.transferAmount * $scope.lessThanOrEqualBorderDayFee / 100 * $scope.discountFeeRate : $scope.minFee;
       // }else {
       //   $scope.counterFee = $scope.transferAmount * $scope.greaterThanBorderDayFee /100 * $scope.discountFeeRate > $scope.minFee ? $scope.transferAmount * $scope.greaterThanBorderDayFee /100 * $scope.discountFeeRate : $scope.minFee;
       // }
-      
+
       // 现规则：手续费 = 转让本金 * 1%，单笔转让的手续费最低为3元。
       $scope.counterFee = $scope.transferAmount * $scope.lessThanOrEqualBorderDayFee / 100 * $scope.discountFeeRate > $scope.minFee ? $scope.transferAmount * $scope.lessThanOrEqualBorderDayFee / 100 * $scope.discountFeeRate : $scope.minFee;
       //待收未收利息
