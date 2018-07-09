8463f9850ce77b1f63d935b9aa309570c99c15ae
yuyang <yuyang1@hooali.com>
2018-07-09 14:17:56 +0800

    fix(ng-src):项目图片首次加载问题

--- app/scripts/controller/project/project-details-ctrl.js
+++ app/scripts/controller/project/project-details-ctrl.js
@@ -168,7 +168,6 @@ angular.module('hongcaiApp')
         if (projectDetails && projectDetails.ret === 1) {
           $rootScope.pageTitle = projectDetails.data.project.name + ' - 宏财网';
           $scope.project = projectDetails.data.project;
-          $scope.baseFileUrl = projectDetails.data.baseFileUrl;
           $scope.repaymentTypeMap = projectDetails.data.repaymentTypeMap;
           /**
            * 预发布倒计时
